07a4e305e20c7c40280bc92b0c3999f27bd73cf3
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-01-25 18:22:33 +0100

    Add snackbar and check if connected

--- apps/script-upload/script-upload/src/app/app.component.ts
+++ apps/script-upload/script-upload/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { AuthQuery } from '@blockframes/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'script-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  constructor(private storage: AngularFireStorage, private auth: AuthQuery) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AuthQuery,
+    private snackBar: MatSnackBar
+  ) {}
 
   public upload(files: FileList) {
+    if (!this.auth.user) {
+      this.snackBar.open('Please login before', 'Ok', { duration: 1500 });
+      return;
+    }
     const file = files.item(0);
     const filePath = `${this.auth.user.uid}/${file.name}`;
     const task = this.storage.upload(filePath, file);
--- apps/script-upload/script-upload/src/app/app.module.ts
+++ apps/script-upload/script-upload/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -12,28 +13,34 @@ import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { environment } from '../environments/environment';
 
+// Material
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 // Libraries
 import { AuthModule } from '@blockframes/auth';
-import { MaterialLayoutModule } from '@blockframes/ui';
 
 // Components
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AuthModule,
-    MaterialLayoutModule,
     FlexLayoutModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([]),
+    // Angular Fire
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireStorageModule,
-    environment.production
-      ? []
-      : [AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot()],
-    BrowserAnimationsModule
+    // Akita
+    environment.production ? [] : [AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot()],
+    // Material
+    MatSnackBarModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
