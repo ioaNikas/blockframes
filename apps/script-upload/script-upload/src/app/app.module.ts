0fefd4aea4661b179264f9971bfff666595f64f3
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-01-25 10:43:34 +0100

    Add akita

--- apps/script-upload/script-upload/src/app/app.module.ts
+++ apps/script-upload/script-upload/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [BrowserModule, NxModule.forRoot(), environment.production ?
+        [] :
+        [ AkitaNgDevtools.forRoot(), AkitaNgRouterStoreModule.forRoot() ]],
   providers: [],
   bootstrap: [AppComponent]
 })
