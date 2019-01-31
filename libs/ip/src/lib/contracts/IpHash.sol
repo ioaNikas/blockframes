1790778d1382991050deb350440532e2f3d18cc8
Laurent Senta <laurent.senta@gmail.com>
2019-01-31 13:27:53 +0000

    script: add Timestamp to ScriptHash

--- libs/ip/src/lib/contracts/IpHash.sol
+++ libs/ip/src/lib/contracts/IpHash.sol
@@ -5,10 +5,13 @@ contract IpHash {
     mapping(address => bytes32[]) private scriptsByOwner;
     mapping(bytes32 => address) public scriptsOwner;
 
+    event Timestamp(bytes32 indexed scriptHash, address indexed owner);
+
     function addIp(bytes32 _hash) public {
         require(scriptsOwner[_hash] == address(0), "Ip has already a owner");
         scriptsOwner[_hash] = msg.sender;
         scriptsByOwner[msg.sender].push(_hash);
+        emit Timestamp(_hash, msg.sender);
     }
 
     function scriptsFrom(address _owner) public view returns (bytes32[] memory) {
