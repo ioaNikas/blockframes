4ce6f0305282738c91e1f41864ebac705beb514a
Laurent Senta <laurent.senta@gmail.com>
2019-02-13 11:20:45 +0100

    add staging conf

---
+++ apps/eth-events-server/src/environments/environment.staging.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  firebase: {
+    apiKey: 'AIzaSyAmos48yDq2xnxy9OPtQpLMiE4NeyJlA5Y',
+    authDomain: 'blockframes-staging.firebaseapp.com',
+    databaseURL: 'https://blockframes-staging.firebaseio.com',
+    projectId: 'blockframes-staging',
+    storageBucket: 'blockframes-staging.appspot.com',
+    messagingSenderId: '176629403574'
+  },
+  network: 'ropsten' as 'ropsten',
+  contracts: {
+    scriptHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185'
+  },
+  pubsub: {
+    projectID: 'blockframes-staging',
+    topicRootName: 'eth-events'
+  }
+};
