3cf714cad71fbd2dbc44eb406cc142286e4ea6a6
Laurent Senta <laurent.senta@gmail.com>
2019-02-13 14:39:54 +0100

    fix env

--- apps/eth-events-server/src/environments/environment.staging.ts
+++ apps/eth-events-server/src/environments/environment.staging.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  hmr: false,
   firebase: {
     apiKey: 'AIzaSyAmos48yDq2xnxy9OPtQpLMiE4NeyJlA5Y',
     authDomain: 'blockframes-staging.firebaseapp.com',
@@ -11,7 +10,7 @@ export const environment = {
   },
   network: 'ropsten' as 'ropsten',
   contracts: {
-    scriptHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185'
+    ipHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185'
   },
   pubsub: {
     projectID: 'blockframes-staging',
--- apps/ip-upload/ip-upload/src/environments/environment.prod.ts
+++ apps/ip-upload/ip-upload/src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
   },
   network: 'ropsten' as 'ropsten',
   contracts: {
-    scriptHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185'
+    ipHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185'
   },
   pubsub: {
     projectID: 'blockframes-laurent-230215',
