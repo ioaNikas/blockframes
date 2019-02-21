d3d8c1f02f87d3beb855203e6da1bd66465a62d2
Laurent Senta <laurent.senta@gmail.com>
2019-02-21 12:26:21 +0100

    first story minimals

--- apps/ip-upload/ip-upload-e2e/src/integration/app.spec.ts
+++ apps/ip-upload/ip-upload-e2e/src/integration/app.spec.ts
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
-
-import { getTitle } from '../support/app.po';
+import { getTitle, Landing } from '../support/app.po';
 
 describe('Hello Nx', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     getTitle().contains('Welcome');
+  });
+});
+
+describe('story 1: je peux me connecter et créer une IP', () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.visit('/');
+    cy.viewport('macbook-15');
+  });
+
+  it('', () => {
+    let p: any = new Landing();
+    p = p.clickConnection();
+    p.fillEmail('laurent+test@singulargarden.com');
+    p.fillPassword('helloworld');
+    p = p.login();
+    p = p.clickNewIp();
+    p.fillTitle('This is my title');
   });
 });
--- apps/ip-upload/ip-upload-e2e/src/support/app.po.ts
+++ apps/ip-upload/ip-upload-e2e/src/support/app.po.ts
@@ -1,3 +1,57 @@
 /// <reference types="cypress" />
 
 export const getTitle = () => cy.get('h1');
+
+export class NewIPPage {
+  constructor() {
+    // TODO: assert it's the right page
+  }
+
+  fillTitle(title: string): any {
+    return cy.get('input[formcontrolname="title"]').type(title);
+  }
+
+  saveStatus() {
+    return cy.get('button').contains('Save');
+  }
+}
+
+export class HomePage {
+  constructor() {
+    // TODO: assert it's the right page
+  }
+
+  clickNewIp(): any {
+    cy.get('mat-toolbar')
+      .contains('New Ip')
+      .click();
+    return new NewIPPage();
+  }
+
+}
+
+export class LandingWithModal {
+  constructor() {
+    cy.contains('Login');
+  }
+
+  public fillEmail(email: string) {
+    cy.get('input[type="email"]').type(email).then(() => this);
+  }
+
+  public fillPassword(password: string) {
+    cy.get('input[type="password"]').type(password).then(() => this);
+  }
+
+  public login(): any {
+    cy.get('button').contains('Login').click();
+    return new HomePage();
+  }
+}
+
+export class Landing {
+  public clickConnection(): any {
+    cy.contains('Connexion').click();
+    return new LandingWithModal();
+  }
+}
