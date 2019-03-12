822e86ec6960842cefdcb4582d75a987069cd0d2
Jackseed <vinkho.c@gmail.com>
2019-03-12 08:08:29 +0100

    movie state: model & service

--- libs/movie/src/lib/+state/movie.service.ts
+++ libs/movie/src/lib/+state/movie.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { MovieStore } from './movie.store';
+import { Movie, createMovie } from './movie.model';
+
 
 @Injectable({ providedIn: 'root' })
 export class MovieService {
+  private collection: AngularFirestoreCollection<Movie>;
 
-  constructor(private movieStore: MovieStore) {
+  constructor(
+  private store: MovieStore,
+  private firestore: AngularFirestore,
+  ) {
+    this.collection = this.firestore.collection('movie');
+  }
+
+  public add(movie: Movie) {
+    const id = this.firestore.createId();
+    this.store.add(createMovie({...movie, id}));
+  }
+
+  public update(id: string, movie: Partial<Movie>) {
+    this.store.update(id, movie);
+  }
+
+  public remove(id: string | string[]) {
+    this.store.remove(id);
   }
 
 }
