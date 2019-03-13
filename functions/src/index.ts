553faf798a8e6d094af0f60cc6b27a1f2c55d10d
Laurent Senta <Laurent.Senta@gmail.com>
2019-03-13 14:55:45 +0100

    orgs: add search user by mail

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -32,3 +32,38 @@ export const onUserCreate = functions.auth
     const { email, uid } = user;
     return db.collection('users').doc(user.uid).set({ email, uid });
   });
+
+/**
+ * Trigger: REST call to find a list of users by email.
+ */
+export const findUserByMail = functions.https
+  .onRequest((req, resp) => {
+    const r = (data: any) => resp.send(JSON.stringify(data));
+
+    const prefix: string = decodeURIComponent(req.query.prefix);
+
+    // Leave if the prefix is too short (do not search every users in the universe).
+    if (prefix.length < 2) {
+      return r({ result: [] });
+    }
+
+    // String magic to figure out a prefixEnd
+    // Say prefix is 'aaa'. We want to search all strings between 'aaa' (prefix) and 'aab' (prefixEnd)
+    // this will match: 'aaa', 'aaaa', 'aaahello', 'aaazerty', etc.
+    const incLast: string = String.fromCharCode(prefix.slice(-1).charCodeAt(0) + 1);
+    const prefixEnd: string = prefix.slice(0, -1) + incLast;
+
+    return db.collection('users')
+      .where('email', '>=', prefix)
+      .where('email', '<', prefixEnd)
+      .get()
+      .then(q => {
+        // leave if there are too many results.
+        if (q.size > 10) {
+          return r({ result: [] });
+        }
+
+        const result = q.docs.map(d => ({ id: d.id, email: d.data().email }));
+        return r({ result });
+      });
+  });
