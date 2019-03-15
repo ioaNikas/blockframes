e78f4d26a23c50201837dfd41ce2d5c749dd22dc
Laurent Senta <Laurent.Senta@gmail.com>
2019-03-15 13:07:18 +0100

    orgs: with data & some style

--- libs/organization/src/lib/+state/org-members.service.ts
+++ libs/organization/src/lib/+state/org-members.service.ts
@@ -3,9 +3,10 @@ import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firest
 import { OrgMembersStore } from './org-members.store';
 import { Organization, OrgMember } from './organization.model';
 import { OrganizationService } from './organization.service';
-import { combineLatest, zip } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map, pluck } from 'rxjs/operators';
 import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { tap } from 'rxjs/internal/operators/tap';
 
 @Injectable({ providedIn: 'root' })
 export class OrgMembersService {
@@ -21,16 +22,16 @@ export class OrgMembersService {
     return this.orgs.addMember(orgID, member);
   }
 
-  public subscribe(orgID: string): void {
-    const pullUserAndOrgRights = (orgID, userID) => (
-      zip(
+  public subscribe(orgID: string): Observable<OrgMember[]> {
+    const pullUserAndOrgRights = (orgID, userID): Observable<OrgMember> => (
+      combineLatest([
         this.firestore.collection('users').doc(userID)
           .get()
           .pipe(map(x => x.data())),
         this.firestore.collection('users').doc(userID).collection('orgRights').doc(orgID)
           .get()
           .pipe(map(x => x.data()))
-      ).pipe(map(([user, rights]): OrgMember => ({
+      ]).pipe(map(([user, rights]): OrgMember => ({
         id: user.uid,
         email: user.email,
         roles: rights.rightNameSlug
@@ -38,15 +39,13 @@ export class OrgMembersService {
     );
 
     // TODO: handle id changes
-    this.collection(orgID)
+    return this.collection(orgID)
       .valueChanges()
       .pipe(
         pluck('userIds'),
-        switchMap(ids => combineLatest(...ids.map(id => pullUserAndOrgRights(orgID, id))))
-      )
-      .subscribe(xs => {
-        this.store.set(xs);
-      });
+        switchMap(ids => combineLatest(...ids.map(id => pullUserAndOrgRights(orgID, id)))),
+        tap(xs => this.store.set(xs))
+      );
   }
 
   private collection(orgID: string): AngularFirestoreDocument<Organization> {
