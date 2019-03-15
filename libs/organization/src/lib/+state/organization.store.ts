ebd8fcff9fc78368cb0cd5426e4e7ac2f1018229
Laurent Senta <Laurent.Senta@gmail.com>
2019-03-15 14:28:14 +0100

    orgs: remove unecessary form

--- libs/organization/src/lib/+state/organization.store.ts
+++ libs/organization/src/lib/+state/organization.store.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
-import { createOrganization, Organization } from './organization.model';
+import { Organization } from './organization.model';
 
 export interface OrganizationState extends EntityState<Organization> {
-  form: Organization;
 }
 
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'organization', idKey: 'id' })
 export class OrganizationStore extends EntityStore<OrganizationState, Organization> {
   constructor() {
-    super({ form: createOrganization() });
+    super({});
   }
 }
