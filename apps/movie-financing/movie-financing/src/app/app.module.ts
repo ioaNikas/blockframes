f903a60e38354941ba7f8b60dc2cdc8a6bf3c06f
Jackseed <vinkho.c@gmail.com>
2019-03-20 14:54:48 +0100

    circle CI fix

--- apps/movie-financing/movie-financing/src/app/app.module.ts
+++ apps/movie-financing/movie-financing/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 // Components
 import { AppComponent } from './app.component';
@@ -18,6 +18,7 @@ import { MovieModule } from '@blockframes/movie';
     BrowserAnimationsModule,
     AppRoutingModule,
     MovieModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
