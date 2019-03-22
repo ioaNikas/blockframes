894d712af609fd58ebdc316220b5c40f7c88ed9d
Hugo <hhontans@cascade8.com>
2019-03-22 14:48:06 +0100

    add template-list

--- apps/delivery/delivery/src/app/template/template.module.ts
+++ apps/delivery/delivery/src/app/template/template.module.ts
@@ -5,27 +5,36 @@ import { RouterModule } from '@angular/router';
 
 // Components
 import { TemplateComponent } from './template/template.component';
-import { TemplateListComponent } from './template-list/template-list.component';
+import { TemplateListComponent, AddTemplateDialog } from './template-list/template-list.component';
 
 // Material
 import {MatCardModule} from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatInputModule, MatMenuModule } from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 
 
 @NgModule({
-  declarations: [TemplateComponent, TemplateListComponent,],
+  declarations: [TemplateComponent, TemplateListComponent, AddTemplateDialog, TemplateFormComponent,],
   imports: [
     CommonModule,
     MatCardModule,
     FlexLayoutModule,
     MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
+    MatMenuModule,
     RouterModule.forChild([
       { path: '', component: TemplateComponent },
       { path: 'list', component: TemplateListComponent },
+      { path: ':templateId', component: TemplateFormComponent },
     ])
-  ]
+  ],
+  entryComponents: [AddTemplateDialog,],
 })
 export class TemplateModule { }
