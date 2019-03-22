e369c4cfba7d1f4cc74b43d1d168573e26dabb20
Hugo <hhontans@cascade8.com>
2019-03-22 14:48:06 +0100

    add template-list

--- apps/delivery/delivery/src/app/template/+state/template.service.ts
+++ apps/delivery/delivery/src/app/template/+state/template.service.ts
@@ -3,28 +3,32 @@ import { OrganizationQuery, OrganizationStore } from '@blockframes/organization'
 import { switchMap, tap, map, filter } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { TemplateStore } from './template.store';
-import { Template } from './template.model';
-
+import { Template, createTemplate } from './template.model';
 
 @Injectable({ providedIn: 'root' })
 export class TemplateService {
-
-  public subscribeOnOrganizationTemplates$ = this.organizationQuery.selectActiveId()
-      .pipe(
-        filter(id => !!id),
-        switchMap(id =>
-          this.db.collection<Template>(`orgs/${id}/templates`).valueChanges()
-        ),
-        tap(templates => this.store.set( templates ))
-      );
-
+  public subscribeOnOrganizationTemplates$ = this.organizationQuery.selectActiveId().pipe(
+    filter(id => !!id),
+    switchMap(id => this.db.collection<Template>(`orgs/${id}/templates`).valueChanges()),
+    tap(templates => this.store.set(templates))
+  );
 
   constructor(
     private organizationQuery: OrganizationQuery,
-    private db : AngularFirestore,
-    private store: TemplateStore,
-    ) {}
+    private db: AngularFirestore,
+    private store: TemplateStore
+  ) {}
 
+  public addTemplate(templateName) {
+    const idOrg = this.organizationQuery.getActiveId();
+    const idTemplate = this.db.createId();
+    const template = createTemplate({ id: idTemplate, name: templateName });
+    this.db.doc<Template>(`orgs/${idOrg}/templates/${idTemplate}`).set(template);
+  }
 
-
+  public deleteTemplate(id) {
+    console.log(id)
+    const idOrg = this.organizationQuery.getActiveId();
+    this.db.doc<Template>(`orgs/${idOrg}/templates/${id}`).delete();
+  }
 }
