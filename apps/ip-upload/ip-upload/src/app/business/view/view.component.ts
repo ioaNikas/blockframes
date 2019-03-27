e78c810ce3efa24debfaa6fc62f074316173b277
bruce <delorme.bruce.michel@gmail.com>
2019-03-27 17:00:40 +0100

    move root components to business module

---
+++ apps/ip-upload/ip-upload/src/app/business/view/view.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IpQuery, Ip } from '@blockframes/ip';
+import { takeWhile } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'business-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent implements OnInit, OnDestroy {
+
+  private alive = true;
+  public ip: Ip;
+
+  constructor(
+    private query: IpQuery,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+
+    this.route.params
+    .pipe(takeWhile(_ => this.alive))
+    .subscribe(params => {
+      this.ip = this.query.getEntity(params.id);
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+
+}
