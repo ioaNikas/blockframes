5a02f8c2b185adce11342b1d2cb894e99cf48406
Laurent Senta <Laurent.Senta@gmail.com>
2019-03-27 15:47:17 +0100

    fix build

--- apps/delivery/delivery/src/app/template/template.module.ts
+++ apps/delivery/delivery/src/app/template/template.module.ts
@@ -2,26 +2,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 // Components
 import { TemplateComponent } from './template/template.component';
-import { TemplateListComponent, AddTemplateDialog } from './template-list/template-list.component';
-
+import { AddTemplateDialogComponent, TemplateListComponent } from './template-list/template-list.component';
 // Material
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatInputModule, MatMenuModule, MatSidenavModule, MatListModule } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDialogModule} from '@angular/material/dialog';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSidenavModule
+} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { MaterialFormComponent } from '../material/material-form/material-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CategoryListComponent, AddCategoryDialog } from './category-list/category-list.component';
-
+import { AddCategoryDialogComponent, CategoryListComponent } from './category-list/category-list.component';
 
 
 @NgModule({
-  declarations: [TemplateComponent, TemplateListComponent, AddTemplateDialog, TemplateFormComponent, MaterialFormComponent, CategoryListComponent, AddCategoryDialog],
+  declarations: [
+    TemplateComponent, TemplateListComponent, AddTemplateDialogComponent,
+    TemplateFormComponent, MaterialFormComponent, CategoryListComponent, AddCategoryDialogComponent
+  ],
   imports: [
     CommonModule,
     MatCardModule,
@@ -38,9 +45,10 @@ import { CategoryListComponent, AddCategoryDialog } from './category-list/catego
     RouterModule.forChild([
       { path: '', redirectTo: 'list', pathMatch: 'full' },
       { path: 'list', component: TemplateListComponent },
-      { path: ':templateId', component: TemplateComponent},
+      { path: ':templateId', component: TemplateComponent }
     ])
   ],
-  entryComponents: [AddTemplateDialog, AddCategoryDialog],
+  entryComponents: [AddTemplateDialogComponent, AddCategoryDialogComponent]
 })
-export class TemplateModule { }
+export class TemplateModule {
+}
