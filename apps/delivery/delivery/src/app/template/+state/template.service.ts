2839ae06b161de961d282b64bcdd638dd856735a
Hugo <hhontans@cascade8.com>
2019-03-27 15:41:47 +0100

    addMaterial, deleteMaterial and updateMaterial work in state, and saveTemplate save in firebase

--- apps/delivery/delivery/src/app/template/+state/template.service.ts
+++ apps/delivery/delivery/src/app/template/+state/template.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 // tslint:disable-next-line
 import { OrganizationQuery } from '@blockframes/organization';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { switchMap, tap, filter, map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { TemplateStore } from './template.store';
 import { createTemplate, Template } from './template.model';
 import { Material, MaterialService } from '../../material/+state';
+import { TemplateQuery } from './template.query';
 
 @Injectable({ providedIn: 'root' })
 export class TemplateService {
@@ -19,9 +20,9 @@ export class TemplateService {
     private organizationQuery: OrganizationQuery,
     private db: AngularFirestore,
     private store: TemplateStore,
-    private materialService: MaterialService
-  ) {
-  }
+    private materialService: MaterialService,
+    private query: TemplateQuery
+  ) {}
 
   public addTemplate(templateName: string) {
     const idOrg = this.organizationQuery.getActiveId();
@@ -48,4 +49,17 @@ export class TemplateService {
     }
   }
 
+  public async saveTemplate() {
+    const idOrg = this.organizationQuery.getActiveId();
+    const template = this.query.getActive();
+    const materials = this.query.unsortedMaterialsByTemplate();
+    const promises: Promise<any>[] = [];
+
+    for (const material of materials) {
+      const promise = this.db.doc<Material>(`orgs/${idOrg}/materials/${material.id}`).set(material);
+      promises.push(promise);
+    }
+    await Promise.all(promises);
+    this.db.doc<Template>(`orgs/${idOrg}/templates/${template.id}`).update({ materialsId: template.materialsId });
+  }
 }
