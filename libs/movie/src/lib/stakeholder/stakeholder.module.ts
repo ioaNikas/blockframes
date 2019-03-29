ee7cb70393cf2e4eccaa2a4ac1932c85d4be61b3
Jackseed <vinkho.c@gmail.com>
2019-03-29 17:31:24 +0100

    WIP title form styling

--- libs/movie/src/lib/stakeholder/stakeholder.module.ts
+++ libs/movie/src/lib/stakeholder/stakeholder.module.ts
@@ -1,12 +1,13 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { StakeholderListComponent } from './list/list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+  ],
   declarations: [StakeholderListComponent]
 })
-
-export class StakholderModule {
-
-  }
+export class StakholderModule {}
