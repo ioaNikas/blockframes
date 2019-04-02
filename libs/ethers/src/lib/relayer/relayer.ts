5c22073c01a9f44ca94ebd448690c5e3ee2be2f5
pldespaigne <pldespaigne@cascade8.com>
2019-04-02 11:54:51 +0200

    change send() types

--- libs/ethers/src/lib/relayer/relayer.ts
+++ libs/ethers/src/lib/relayer/relayer.ts
@@ -1,12 +1,13 @@
 import { MetaTransaction } from '../erc1077/meta-transaction';
 import { Injectable } from '@angular/core';
+import { providers } from 'ethers';
 
 export interface IRelayer {
   create: (name: string, key: string) => Promise<Object>;
 
   prepare: (name: string) => Promise<Object>;
 
-  send: (name: string, tx: MetaTransaction) => Promise<Object>;
+  send: (name: string, tx: providers.TransactionRequest) => Promise<providers.TransactionResponse>;
 
   addKey: (name: string, key: string) => Promise<Object>;
 
@@ -16,18 +17,18 @@ export interface IRelayer {
 @Injectable({ providedIn: 'root' })
 export class Relayer implements IRelayer {
   async create(name: string, key: string) {
-    return { message: `mock erc1077 created` };
+    return { message: 'mock erc1077 created' };
   }
   async prepare(name: string) {
-    return { message: `mock erc1077 prepare` };
+    return { message: 'mock erc1077 prepare' };
   }
-  async send(name: string, tx: MetaTransaction) {
-    return { message: `mock erc1077 send` };
+  async send(name: string, tx: providers.TransactionRequest) {
+    return <providers.TransactionResponse>{ data: 'mock erc1077 send'};
   }
   async addKey(name: string, key: string) {
-    return { message: `mock erc1077 addKey` };
+    return { message: 'mock erc1077 addKey' };
   }
   async removeKey(name: string, key: string) {
-    return { message: `mock erc1077 removeKey` };
+    return { message: 'mock erc1077 removeKey' };
   }
 }
