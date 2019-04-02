f70377da4c864cf9a107f551b0d38bc718728992
pldespaigne <pldespaigne@cascade8.com>
2019-04-02 10:56:17 +0200

    mock relayer implementation

--- libs/ethers/src/lib/relayer/relayer.ts
+++ libs/ethers/src/lib/relayer/relayer.ts
@@ -1,6 +1,7 @@
 import { MetaTransaction } from '../erc1077/meta-transaction';
+import { Injectable } from '@angular/core';
 
-export interface Relayer {
+export interface IRelayer {
   create: (name: string, key: string) => Promise<Object>;
 
   prepare: (name: string) => Promise<Object>;
@@ -10,4 +11,23 @@ export interface Relayer {
   addKey: (name: string, key: string) => Promise<Object>;
 
   removeKey: (name: string, key: string) => Promise<Object>;
+}
+
+@Injectable({ providedIn: 'root' })
+export class Relayer implements IRelayer {
+  async create(name: string, key: string) {
+    return { message: `mock erc1077 created` };
+  }
+  async prepare(name: string) {
+    return { message: `mock erc1077 prepare` };
+  }
+  async send(name: string, tx: MetaTransaction) {
+    return { message: `mock erc1077 send` };
+  }
+  async addKey(name: string, key: string) {
+    return { message: `mock erc1077 addKey` };
+  }
+  async removeKey(name: string, key: string) {
+    return { message: `mock erc1077 removeKey` };
+  }
 }
