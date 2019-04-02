e5f32b3424aad845b0b5d8b3333310d3b0062960
pldespaigne <pldespaigne@cascade8.com>
2019-04-02 16:32:18 +0200

    add mock relayer implementation

--- libs/ethers/src/lib/relayer/relayer.ts
+++ libs/ethers/src/lib/relayer/relayer.ts
@@ -1,6 +1,7 @@
 import { MetaTransaction } from '../erc1077/meta-transaction';
 import { Injectable } from '@angular/core';
 import { providers } from 'ethers';
+import { BigNumber } from 'ethers/utils';
 
 export interface IRelayer {
   create: (name: string, key: string) => Promise<Object>;
@@ -15,15 +16,31 @@ export interface IRelayer {
 }
 
 @Injectable({ providedIn: 'root' })
-export class Relayer implements IRelayer {
+export class MockRelayer implements IRelayer {
   async create(name: string, key: string) {
     return { message: 'mock erc1077 created' };
   }
   async prepare(name: string) {
     return { message: 'mock erc1077 prepare' };
   }
-  async send(name: string, tx: providers.TransactionRequest) {
-    return <providers.TransactionResponse>{ data: 'mock erc1077 send'};
+  async send(
+    name: string,
+    tx: providers.TransactionRequest
+  ): Promise<providers.TransactionResponse> {
+    const receipt: providers.TransactionReceipt = {
+      byzantium: false
+    };
+    return {
+      confirmations: 0,
+      from: '0x0',
+      wait: async () => receipt,
+      nonce: 0,
+      gasLimit: new BigNumber(0),
+      gasPrice: new BigNumber(0),
+      data: '0x0',
+      value: new BigNumber(0),
+      chainId: 0
+    };
   }
   async addKey(name: string, key: string) {
     return { message: 'mock erc1077 addKey' };
