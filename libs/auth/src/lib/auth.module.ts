046c07a865790194c6dcfecc230af7d91b61e5f3
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-03 14:43:42 +0200

    Quick fix and formating

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -20,24 +20,11 @@ import { MatMenuModule } from '@angular/material/menu';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { MatCardModule } from '@angular/material';
+import { AuthRootComponent } from './root/root.component';
 
 
 export const AuthRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-    data: { fallback: '', org: null }
-  },
-  {
-    path: 'signup',
-    component: SignupComponent,
-    data: { fallback: '', org: null }
-  }
+  { path: '', component: AuthRootComponent }
 ];
 
 @NgModule({
@@ -58,7 +45,7 @@ export const AuthRoutes: Routes = [
     RouterModule.forChild(AuthRoutes),
   ],
   entryComponents: [SignupComponent, LoginComponent],
-  declarations: [SignupComponent, LoginComponent],
+  declarations: [SignupComponent, LoginComponent, AuthRootComponent],
   exports: [SignupComponent, LoginComponent],
 })
 export class AuthModule {}
