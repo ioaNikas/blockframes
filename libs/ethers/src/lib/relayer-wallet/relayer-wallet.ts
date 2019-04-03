7dc4711d6f8cb7774fe0ef47c1339f8be0c242ec
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-03 17:57:38 +0200

    Add comment and login to RelayerWallet

--- libs/ethers/src/lib/relayer-wallet/relayer-wallet.ts
+++ libs/ethers/src/lib/relayer-wallet/relayer-wallet.ts
@@ -1,32 +1,60 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ethers, utils, Wallet, providers } from 'ethers';
 
 import { Provider } from '../provider/provider';
 import { Relayer } from '../relayer/relayer';
+import { Vault } from '../vault/vault';
+
+type txRequest = providers.TransactionRequest;
+type txResponse = providers.TransactionResponse;
 
 @Injectable({ providedIn: 'root' })
 export class RelayerWallet extends ethers.Signer {
   private signingKey: utils.SigningKey;
   public username: string;
 
-  constructor(public provider: Provider, private relayer: Relayer) {
+  constructor(private vault: Vault, private relayer: Relayer, public provider: Provider) {
     super();
   }
 
-  async loginFromEncryptedJSON(username: string, encryptedJSON: string, password: string) {
+  /** Set into process memory */
+  private loginWithEncryptedJSON(wallet: Wallet, username: string) {
+    this.username = username;
+    this.signingKey = new utils.SigningKey(wallet.privateKey);
+  }
+
+  /** Save the encryptedJSON into the vault */
+  private async saveIntoVault(wallet: Wallet, username: string, password: string) {
+    const encryptedJSON = await wallet.encrypt(password);
+    this.vault.set(`${username}:web`, encryptedJSON);
+  }
+
+  /** Get the signing key from an encrypted JSON */
+  async login(username: string, password: string) {
     if (password.length < 5) throw new Error('Password must contains at least 6 charachters');
-    this.username = username;
+    const encryptedJSON = await this.vault.get(`${username}:web`);
     const wallet = await Wallet.fromEncryptedJson(encryptedJSON, password);
-    this.signingKey = new utils.SigningKey(wallet.privateKey);
+    this.loginWithEncryptedJSON(wallet, username);
   }
-  async loginFromMnemonic(username: string, mnemonic: string) {
-    this.username = username;
+
+  /** Get the signing key from a mnemonic */
+  async importMnemonic(mnemonic: string, username: string, password: string) {
     const wallet = Wallet.fromMnemonic(mnemonic);
-    this.signingKey = new utils.SigningKey(wallet.privateKey);
+    this.loginWithEncryptedJSON(wallet, username);
+    this.saveIntoVault(wallet, username, password);
   }
+
+  /** Create a wallet and store it into the vault */
+  async signup(username: string, password: string) {
+    const wallet = Wallet.createRandom();
+    this.loginWithEncryptedJSON(wallet, username);
+    this.saveIntoVault(wallet, username, password);
+  }
+
+  /** Forget the signing key in the process memory */
   logout() {
-    this.username = undefined;
-    this.signingKey = undefined;
+    delete this.username;
+    delete this.signingKey;
   }
 
   async getAddress(): Promise<string> {
@@ -36,7 +64,7 @@ export class RelayerWallet extends ethers.Signer {
   async signMessage(message: utils.Arrayish | string): Promise<string> {
     // Here ask permission
     const msg =
-      (typeof message === 'string') && (message.slice(0, 2) === '0x')
+      typeof message === 'string' && message.slice(0, 2) === '0x'
         ? utils.arrayify(message)
         : message;
     const hash = utils.hashMessage(msg);
@@ -44,9 +72,7 @@ export class RelayerWallet extends ethers.Signer {
     return utils.joinSignature(signature);
   }
 
-  async sendTransaction(
-    transaction: providers.TransactionRequest
-  ): Promise<providers.TransactionResponse> {
+  async sendTransaction(transaction: txRequest): Promise<txResponse> {
     return this.relayer.send(this.username, transaction);
   }
 }
