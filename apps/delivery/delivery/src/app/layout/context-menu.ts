5793a1d2fc2ad0674015f20fdfda8c7967e484c2
Ioannis Nikas <ioa.nikas@gmail.com>
2019-04-04 10:23:45 +0200

    Routing: configured new context menu to match delivery app routes

--- apps/delivery/delivery/src/app/layout/context-menu.ts
+++ apps/delivery/delivery/src/app/layout/context-menu.ts
@@ -1,12 +1,12 @@
 export const CONTEXT_MENU = [
   {route: 'default', items: [
-    {name: 'explorer', path: '/layout/home'},
-    {name: 'example', path: '/layout/account/profile'},
+    {name: 'home', path: '/layout/home'},
+    {name: 'template', path: '/layout/template'},
   ]},
   {route: '/layout/:id', items: [
-    {name: 'explorer', path: '/layout/home', exact: true},
-    {name: 'view', path: '/layout/:id', exact: true},
-    {name: 'edit', path: '/layout/home/form/:id/edit', exact: true}
+    {name: 'home', path: '/layout/home', exact: true},
+    {name: 'view', path: '/layout/:id/movie-materials', exact: true},
+    {name: 'list', path: '/layout/:id', exact: true}
   ]},
   {route: '/layout/home/form/:id', items: [
     {name: 'explorer', path: '/layout/home', exact: true},
