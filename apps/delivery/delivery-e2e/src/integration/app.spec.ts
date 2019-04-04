c886597f95218309d5d617f22da63ea4a80a52a3
Ioannis Nikas <ioa.nikas@gmail.com>
2019-04-04 10:18:12 +0200

    Fix: resolved conflits on rebase

--- apps/delivery/delivery-e2e/src/integration/app.spec.ts
+++ apps/delivery/delivery-e2e/src/integration/app.spec.ts
@@ -1,9 +1,40 @@
-import { getGreeting } from '../support/app.po';
+/// <reference types="cypress" />
+import { getTitle, LandingPage } from '../support/app.po';
 
-describe('Hello Nx', () => {
-  beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to delivery!');
+let currentID = 0;
+
+const randomID = (): string => (`${new Date().toISOString()}-${currentID++}`);
+
+const createFakeScript = (title: string): any => cy.task('random:pdf', title);
+
+beforeEach(() => {
+  cy.clearCookies();
+  cy.visit('/');
+  cy.viewport('macbook-15');
+});
+
+describe('Hello Delivery', () => {
+  it('should display login message', () => {
+    getTitle().contains('Please Login');
   });
 });
+
+describe('i m a user and i can create an empty template', () => {
+  it('', () => {
+    let p: any = new LandingPage();
+    p.fillEmail('laurent+test@singulargarden.com');
+    p.fillPassword('helloworld');
+    p = p.login();
+    p.displayUserMenu();
+    p.selectOrganization('Org1');
+    cy.visit('/');
+    p = p.selectTemplate();
+    p = p.createTemplate();
+    const templateName= randomID();
+    p.fillTemplateName(templateName);
+    p = p.clickCreate();
+    p.assertTemplateExists(templateName);
+    //p.clickMenu(templateName);
+  })
+})
--- apps/delivery/delivery-e2e/src/support/app.po.ts
+++ apps/delivery/delivery-e2e/src/support/app.po.ts
@@ -1,1 +1,79 @@
 export const getGreeting = () => cy.get('h1');
+/// <reference types="cypress" />
+
+export const getTitle = () => cy.get('mat-card-title');
+
+export class AddTemplateModal {
+  constructor() {
+    //TODO: verify that we are in the correct page
+  }
+
+  public fillTemplateName(name: string) {
+    cy.get('input[placeholder="New template name"]').type(name);
+  }
+
+  public clickCreate() {
+    cy.get('button').contains('Create').click();
+// tslint:disable-next-line: no-use-before-declare
+    return new TemplateListPage();
+  }
+}
+
+export class TemplateListPage {
+  constructor() {
+    //TODO: verify that we are in the correct page
+  }
+
+  public createTemplate() {
+    cy.get('button').contains('Add Template').click();
+    return new AddTemplateModal();
+  }
+
+  public assertTemplateExists(name: string) {
+    cy.get('template-item').contains(name).should('have.length', 1);
+  }
+
+  public clickMenu(name: string) {
+    cy.get('mat-card').should('contain', name).contains(name).get('mat-icon').contains('more_vert')
+    .click();
+  }
+}
+
+export class HomePage {
+  constructor() {
+    //TODO: verify that we are in the correct page
+  }
+
+  public displayUserMenu() {
+    cy.get('mat-icon').contains('account_circle').click();
+  }
+
+  public selectOrganization(name: string) {
+    cy.get('button').should('contain', name).contains(name).click();
+  }
+
+  public selectTemplate() {
+    cy.get('.top-toolbar-context-menu').get('a').contains('Template').click();
+    return new TemplateListPage();
+  }
+}
+
+export class LandingPage {
+  constructor() {
+    cy.contains('Login');
+  }
+
+  public fillEmail(email: string) {
+    cy.get('input[type="email"]').type(email);
+  }
+
+  public fillPassword(password: string) {
+    cy.get('input[type="password"]').type(password);
+  }
+
+  public login(): any {
+    cy.get('button').contains('Login').click();
+    return new HomePage();
+  }
+
+}
