9c7ac76fb7090725270e5d7323648c752a163990
Jackseed <vinkho.c@gmail.com>
2019-04-08 16:34:58 +0200

    fetchMoviesOrgs$ function to update movie store with orgId

--- libs/movie/src/lib/movie/+state/movie.model.ts
+++ libs/movie/src/lib/movie/+state/movie.model.ts
@@ -1,6 +1,7 @@
 export interface Movie {
   id: string,
   title: string[],
+  orgId?: string,
   ipId: string,
   credits: {firstName: string, lastName: string, creditRole: string}[],
   stakeholderIds: string[],
@@ -30,8 +31,8 @@ export interface Movie {
  * A factory function that creates Movie
  */
 export function createMovie(params?: Partial<Movie>) {
-  return params ? {
+  return {
     stakeholderIds: [],
     ...params,
-  } : {} as Movie;
+  } as Movie;
 }
