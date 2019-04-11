2199be55c05c8dcfacc5c32a2136ba26606fd1c1
Ioannis Nikas <ioa.nikas@gmail.com>
2019-04-11 10:13:09 +0200

    Routes: modified routes in context menu

--- apps/delivery/delivery/src/app/layout/context-menu.ts
+++ apps/delivery/delivery/src/app/layout/context-menu.ts
@@ -1,30 +1,55 @@
 export const CONTEXT_MENU = [
   {
     route: 'default',
-    items: [{ name: 'home', path: '/layout/home' }, { name: 'template', path: '/layout/template' }]
+    items: [{ name: 'home', path: '/layout/home' }, { name: 'templates', path: '/layout/template' }]
   },
   {
     route: '/layout/:movieId',
     items: [
       { name: 'home', path: '/layout/home'},
-      { name: 'view', path: '/layout/:movieId/movie-materials'},
-      { name: 'list', path: '/layout/:movieId'}
+      { name: 'deliveries', path: '/layout/:movieId'},
+      { name: 'movie materials', path: '/layout/:movieId/movie-materials'},
+    ]
+  },
+  {
+    route: '/layout/:movieId/movie-materials',
+    items: [
+      { name: 'home', path: '/layout/home'},
+      { name: 'deliveries', path: '/layout/:movieId'},
+      { name: 'movie materials', path: '/layout/:movieId/movie-materials'},
     ]
   },
   {
     route: '/layout/:movieId/view/:deliveryId',
     items: [
       { name: 'home', path: '/layout/home'},
-      { name: 'delivery', path: '/layout/:movieId/view/:deliveryId'},
-      { name: 'list', path: '/layout/:movieId', exact: true}, 
+      { name: 'deliveries', path: '/layout/:movieId', exact: true},
+      { name: 'movie materials', path: '/layout/:movieId/movie-materials'},
+      { name: 'delivery', path: '/layout/:movieId/view/:deliveryId'}
     ]
   },
   {
     route: '/layout/:movieId/form/:deliveryId',
     items: [
       { name: 'home', path: '/layout/home'},
-      { name: 'delivery', path: '/layout/:movieId/form/:deliveryId'},
-      { name: 'list', path: '/layout/:movieId', exact: true}
+      { name: 'deliveries', path: '/layout/:movieId', exact: true},
+      { name: 'movie materials', path: '/layout/:movieId/movie-materials'},
+      { name: 'delivery', path: '/layout/:movieId/form/:deliveryId'}
     ]
-  }
+  },
+  {
+    route: '/layout/template/list',
+    items: [
+      { name: 'home', path: '/layout/home'},
+      { name: 'templates', path: '/layout/template/list'},
+    ]
+  },
+  {
+    route: '/layout/template/:templateId',
+    items: [
+      { name: 'home', path: '/layout/home'},
+      { name: 'templates', path: '/layout/template/list'},
+      { name: 'edit', path: '/layout/template/:templateId'}
+    ]
+  },
 ];
