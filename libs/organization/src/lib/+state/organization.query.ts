f04f00a58eb8c7b3f936380d3e9d008c4c72f159
Hugo <hhontans@cascade8.com>
2019-04-12 12:06:30 +0200

    clear code getOrgId()

--- libs/organization/src/lib/+state/organization.query.ts
+++ libs/organization/src/lib/+state/organization.query.ts
@@ -2,24 +2,19 @@ import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
 import { OrganizationState, OrganizationStore } from './organization.store';
 import { Organization } from './organization.model';
-import { map, switchMap } from 'rxjs/operators';
-import { combineLatest, Observable } from 'rxjs';
-import { Template } from '@blockframes/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrganizationQuery extends QueryEntity<OrganizationState, Organization> {
-  constructor(protected store: OrganizationStore) {
+  constructor(
+    protected store: OrganizationStore,
+    ) {
     super(store);
   }
 
   public getOrgId(name: string) {
-    const orgs = this.getAll({
-      filterBy: org => org.name === name,
-      limitTo: 0
-    })
-    return orgs[0].id;
+    return this.getAll().find(org => org.name === name).id;
   }
 
   get form$() {
