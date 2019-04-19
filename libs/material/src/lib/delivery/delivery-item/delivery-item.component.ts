192ff9f70d95f6c826e9b4974b85943c0e43e894
Ioannis Nikas <ioa.nikas@gmail.com>
2019-04-19 17:43:17 +0200

    Style: New style for movie materials

--- libs/material/src/lib/delivery/delivery-item/delivery-item.component.ts
+++ libs/material/src/lib/delivery/delivery-item/delivery-item.component.ts
@@ -9,8 +9,6 @@ import {
 import { Delivery } from '../+state/delivery.model';
 import { Observable } from 'rxjs';
 import { DeliveryQuery } from '../+state';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'delivery-item',
@@ -27,44 +25,12 @@ export class DeliveryItemComponent implements OnInit {
   // Visual bullshit for WoW effect
   public stateIcons = ['dummy', 'accepted', 'available', 'delivered', 'pending', 'refused'];
   public paymentIcons = ['dummy', 'payed', 'not_payed'];
-
   public stateIcon: string;
   public paymentIcon: string;
 
   constructor(
     private query: DeliveryQuery,
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'accepted',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/accepted.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'available',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/available.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'delivered',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/delivered.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'not_payed',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/not-payed.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'payed',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/payed.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'pending',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/pending.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'refused',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/refused.svg')
-    );
-  }
+  ) {}
 
   ngOnInit() {
     this.progression$ = this.query.deliveryProgressionById(this.delivery.id);
--- libs/material/src/lib/delivery/movie-materials/movie-materials.component.ts
+++ libs/material/src/lib/delivery/movie-materials/movie-materials.component.ts
@@ -1,13 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Location } from '@angular/common';
 import { Material } from '../../material/+state';
 import { Movie } from 'libs/movie/src/lib/movie/+state/movie.model';
 import { MovieQuery } from 'libs/movie/src/lib/movie/+state/movie.query';
 import { DeliveryService } from '../+state/delivery.service';
 import { DeliveryQuery } from '../+state';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'delivery-movie-materials',
@@ -21,72 +18,24 @@ export class MovieMaterialsComponent implements OnInit {
   public materials$: Observable<Object>;
   public progressionValue$: Observable<number>;
 
+  // Visual bullshit for WoW effect
+  public stateIcons = ['dummy', 'accepted', 'available', 'delivered', 'pending', 'refused'];
+  public paymentIcons = ['dummy', 'payed', 'not_payed'];
+  public stateIcon: string;
+  public paymentIcon: string;
+
   constructor(
     private movieQuery: MovieQuery,
-    private location: Location,
     private deliveryService: DeliveryService,
     private deliveryQuery: DeliveryQuery,
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-    ) {
-      this.matIconRegistry.addSvgIcon(
-        'accepted',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/accepted.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'available',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/available.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'delivered',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/delivered.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'disabled',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/disabled.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'enabled',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/remove_red_eye.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'filter',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/filter.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'library_books',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/library_books.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'not_payed',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/not-payed.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'order',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/order.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'payed',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/payed.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'pending',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/pending.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'picture_as_pdf',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/picture_as_pdf.svg')
-      );
-      this.matIconRegistry.addSvgIcon(
-        'refused',
-        this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/refused.svg')
-      );
-     }
+    ) {}
 
   ngOnInit() {
     this.movie$ = this.movieQuery.selectActive();
     this.materials$ = this.deliveryQuery.materialsByActiveMovie$;
     this.progressionValue$ = this.deliveryQuery.movieProgression$;
+    this.stateIcon = this.stateIcons[this.randomNumberPicker(5)];
+    this.paymentIcon = this.paymentIcons[this.randomNumberPicker(2)];
   }
 
   public deliveredToggle(material: Material, movieId: string) {
@@ -95,8 +44,8 @@ export class MovieMaterialsComponent implements OnInit {
       .catch(err => console.log(err));
   }
 
-  public goBack() {
-    this.location.back();
+  public randomNumberPicker(scale: number) {
+    return Math.floor(Math.random() * scale) + 1;
   }
 
 }
