905ffe8bc7688fa2f70350cf9bb922ddace8d5c0
Ioannis Nikas <ioa.nikas@gmail.com>
2019-04-24 12:32:56 +0200

    wip

--- libs/ui/src/lib/notification/+state/notification.model.ts
+++ libs/ui/src/lib/notification/+state/notification.model.ts
@@ -1,12 +1,23 @@
 // tslint:disable-next-line: interface-over-type-literal
 export type Notification = {
   id: string;
-  content: string;
   app: string;
+  message: string;
+  orgId: string[];
+  isRead: boolean;
+  date: number;
 };
 
-export function createDelivery(params: Partial<Notification>) {
-  return {
-    ...params
-  } as Notification;
+/**
+ * A factory function that creates a Notification
+ */
+export function createNotification(params?: Partial<Notification>): Notification {
+  return params ? {
+    id: params.id || '',
+    app: params.app || 'Delivery', //! Delivery only here to mock the data
+    message: params.message,
+    orgId: params.orgId,
+    isRead: params.isRead || false,
+    date: params.date || Date.now(),
+  } : {} as Notification;
 }
--- libs/ui/src/lib/notification/+state/notification.service.ts
+++ libs/ui/src/lib/notification/+state/notification.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@angular/core';
+import { OrganizationQuery } from '@blockframes/organization';
+import { FireQuery, Query } from '@blockframes/utils';
+import { switchMap, combineLatest, map, takeWhile, tap } from 'rxjs/operators';
+import { NotificationStore } from './notification.store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeliveryService {
   constructor(
+    private orgQuery: OrganizationQuery,
+    private fireQuery: FireQuery,
+    private store: NotificationStore,
+    ) {}
 
-  ) {}
+  public get orgNotificationList() {
+    return this.orgQuery.selectAll().pipe(
+      switchMap(orgs => {
+        const notifications$ = orgs.map(org => {
+          return this.fireQuery.fromQuery(this.getNotificationsByOrgIds(org.id))
+        });
+        return notifications$;
+      }),
+      tap((notifications: Notification[]) => this.store.set(notifications))
+    )
+  }
+
+  private getNotificationsByOrgIds(orgId: string): Query<Notification[]> {
+    return <Query<Notification[]>> {
+      path: `notifications`,
+      queryFn: ref => ref.where('orgId', '==', orgId),
+    };
+  }
 }
