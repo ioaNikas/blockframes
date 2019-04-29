d7456140471533a5b5665466036987324ccf18d9
pldespaigne <pl.despaigne@gmail.com>
2019-04-29 12:29:45 +0200

    fix provider bug

--- libs/ethers/src/lib/provider/provider.ts
+++ libs/ethers/src/lib/provider/provider.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { network } from '@env';
 
-import { providers } from 'ethers';
+import { providers, getDefaultProvider } from 'ethers';
 
 @Injectable({ providedIn: 'root' })
-export class Provider extends providers.BaseProvider {
+export class Provider extends providers.FallbackProvider {
   constructor() {
-    super(network);
+    super([getDefaultProvider(network)]);
   }
 }
