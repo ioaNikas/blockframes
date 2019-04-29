5dfb334e2b7a5a28555f7d76718daf80f1044cc9
pldespaigne <pl.despaigne@gmail.com>
2019-04-29 12:31:57 +0200

    add wallet info

--- libs/account/src/lib/account-view/account-view.component.ts
+++ libs/account/src/lib/account-view/account-view.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-
-import { PersistNgFormPlugin } from '@datorama/akita';
 import { Observable } from 'rxjs';
 import { User, AuthQuery } from '@blockframes/auth';
+import { network, baseEnsDomain } from '@env';
+import { map, filter } from 'rxjs/operators';
+import * as makeBlockie from 'ethereum-blockies-base64';
 
 @Component({
   selector: 'account-view',
@@ -12,12 +13,22 @@ import { User, AuthQuery } from '@blockframes/auth';
 })
 export class AccountViewComponent implements OnInit, OnDestroy {
   public user$: Observable<User>;
+  public asciiUsername$: Observable<string>;
+  public blockie$: Observable<string>;
+  public basedomain: string;
+  public network: string;
 
   constructor( private authQuery: AuthQuery) {
   }
 
   ngOnInit() {
     this.user$ = this.authQuery.user$;
+    this.blockie$ = this.user$.pipe(
+      filter(user => !!user),
+      map(user => makeBlockie.default(user.identity.address))
+    );
+    this.basedomain = baseEnsDomain;
+    this.network = network ;
   }
 
   ngOnDestroy() {
