7fb3bf017865b81cd6c6af7232b459fca95432aa
bruce <delorme.bruce.michel@gmail.com>
2019-04-30 12:46:08 +0200

    moved form preview to a child component

--- libs/movie/src/lib/movie/movie.module.ts
+++ libs/movie/src/lib/movie/movie.module.ts
@@ -34,6 +34,7 @@ import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
 import { ViewComponent } from './view/view.component';
+import { PreviewComponent } from './form/preview.component';
 import { MovieRoutingModule } from './movie-routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { TitleFormComponent } from '../movie/title-form/title-form.component';
