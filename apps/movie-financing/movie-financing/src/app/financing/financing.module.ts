119be19e6a50c6d6f7839a49c7abe15edfdd4617
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-03 17:02:11 +0200

    financing(dashboard): prep routes

--- apps/movie-financing/movie-financing/src/app/financing/financing.module.ts
+++ apps/movie-financing/movie-financing/src/app/financing/financing.module.ts
@@ -1,15 +1,30 @@
 // Angular
 import { CommonModule } from '@angular/common';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   providers: [],
   bootstrap: []
 })
-export class FinancingModule {}
+export class FinancingModule {
+}
