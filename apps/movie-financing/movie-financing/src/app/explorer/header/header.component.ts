b44e2e069cbb6d91b3cb77724a1d0d5feeb65e41
pldespaigne <pl.despaigne@gmail.com>
2019-05-06 12:50:32 +0200

    use ng-content

--- apps/movie-financing/movie-financing/src/app/explorer/header/header.component.ts
+++ apps/movie-financing/movie-financing/src/app/explorer/header/header.component.ts
@@ -7,15 +7,6 @@ import { ChangeDetectionStrategy, Component, OnInit, Input } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FinancingExplorerHeaderComponent implements OnInit {
-
-  @Input() title = 'Annette';
-  @Input() subtitle = 'Leos Carax';
-  @Input() label = 'logline';
-  @Input() content = 'A stand-up comedian, and his opera singer wife, have a 2 year old daughter with a surprising gift.';
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+  constructor() {}
+  ngOnInit() {}
 }
