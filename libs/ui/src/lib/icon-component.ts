0d376e8c3b635b7e32b866f90fa57a24fd4ba2b7
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-06 14:55:06 +0200

    ui(icons): factor out svg icon setup

--- libs/ui/src/lib/icon-component.ts
+++ libs/ui/src/lib/icon-component.ts
@@ -2,6 +2,77 @@ import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Injectable } from '@angular/core';
 
+const ICONS_SVG = [
+  {
+    name: 'accepted',
+    url: 'assets/icons/accepted.svg'
+  },
+  {
+    name: 'applications',
+    url: 'assets/apps/applications.svg'
+  },
+  {
+    name: 'available',
+    url: 'assets/icons/available.svg'
+  },
+  {
+    name: 'delivered',
+    url: 'assets/icons/delivered.svg'
+  },
+  {
+    name: 'delivery',
+    url: 'assets/apps/delivery.svg'
+  },
+  {
+    name: 'disabled',
+    url: 'assets/icons/disabled.svg'
+  },
+  {
+    name: 'enabled',
+    url: 'assets/icons/remove_red_eye.svg'
+  },
+  {
+    name: 'filter',
+    url: 'assets/icons/filter.svg'
+  },
+  {
+    name: 'library_books',
+    url: 'assets/icons/library_books.svg'
+  },
+  {
+    name: 'moviefinancing',
+    url: 'assets/apps/moviefinancing.svg'
+  },
+  {
+    name: 'notifications',
+    url: 'assets/icons/notifications.svg'
+  },
+  {
+    name: 'order',
+    url: 'assets/icons/order.svg'
+  },
+  {
+    name: 'not_payed',
+    url: 'assets/icons/not-payed.svg'
+  },
+  {
+    name: 'payed',
+    url: 'assets/icons/payed.svg'
+  },
+  {
+    name: 'pending',
+    url: 'assets/icons/pending.svg'
+  },
+  {
+    name: 'picture_as_pdf',
+    url: 'assets/icons/picture_as_pdf.svg'
+  },
+  {
+    name: 'refused',
+    url: 'assets/icons/refused.svg'
+  }
+];
+
 /**
  * Load the icons and make sure they are provided everywhere.
  * To be used at the root of every app.
@@ -14,73 +85,8 @@ export class IconComponent {
   constructor(private matIconRegistry: MatIconRegistry,
               private domSanitizer: DomSanitizer
   ) {
-    this.matIconRegistry.addSvgIcon(
-      'accepted',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/accepted.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'applications',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/apps/applications.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'available',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/available.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'delivered',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/delivered.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'delivery',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/apps/delivery.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'disabled',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/disabled.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'enabled',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/remove_red_eye.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'filter',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/filter.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'library_books',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/library_books.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'moviefinancing',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/apps/moviefinancing.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'notifications',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/notifications.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'order',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/order.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'not_payed',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/not-payed.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'payed',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/payed.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'pending',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/pending.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'picture_as_pdf',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/picture_as_pdf.svg')
-    );
-    this.matIconRegistry.addSvgIcon(
-      'refused',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/refused.svg')
-    );
+    ICONS_SVG.forEach(({ name, url }) => {
+      this.matIconRegistry.addSvgIcon(name, this.domSanitizer.bypassSecurityTrustResourceUrl(url));
+    });
   }
 }
