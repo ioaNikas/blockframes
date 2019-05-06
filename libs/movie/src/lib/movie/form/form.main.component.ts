e7d96177242ef59571a3da1d2c8ef62d58ea75d3
bruce <delorme.bruce.michel@gmail.com>
2019-05-06 12:19:19 +0200

    CSS, renamed observables and small corrections

--- libs/movie/src/lib/movie/form/form.main.component.ts
+++ libs/movie/src/lib/movie/form/form.main.component.ts
@@ -23,10 +23,10 @@ export class FormMainComponent implements OnInit, OnDestroy {
   public languagesFilterCtrl: FormControl = new FormControl();
   public genresFilterCtrl: FormControl = new FormControl();
   public audiovisual_typesFilterCtrl: FormControl = new FormControl();
-  public countries: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
-  public languages: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
-  public genres: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
-  public audiovisual_types: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  public countries$: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  public languages$: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  public genres$: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  public audiovisual_types$: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
   
   constructor() {}
 
@@ -38,22 +38,22 @@ export class FormMainComponent implements OnInit, OnDestroy {
   /* Selects with search bar */
   private _selectSearchSubScriptions() : void {
     
-    this.countries.next(staticModels.COUNTRIES.slice());
+    this.countries$.next(staticModels.COUNTRIES.slice());
     this.countriesFilterCtrl.valueChanges
     .pipe(takeWhile(() => this.isAlive))
     .subscribe(() => this.filterSelectSearch('COUNTRIES'));
 
-    this.languages.next(staticModels.LANGUAGES.slice());
+    this.languages$.next(staticModels.LANGUAGES.slice());
     this.languagesFilterCtrl.valueChanges
     .pipe(takeWhile(() => this.isAlive))
     .subscribe(() => this.filterSelectSearch('LANGUAGES'));
 
-    this.genres.next(staticModels.GENRES.slice());
+    this.genres$.next(staticModels.GENRES.slice());
     this.genresFilterCtrl.valueChanges
     .pipe(takeWhile(() => this.isAlive))
     .subscribe(() => this.filterSelectSearch('GENRES'));
 
-    this.audiovisual_types.next(staticModels.AUDIOVISUAL_TYPES.slice());
+    this.audiovisual_types$.next(staticModels.AUDIOVISUAL_TYPES.slice());
     this.audiovisual_typesFilterCtrl.valueChanges
     .pipe(takeWhile(() => this.isAlive))
     .subscribe(() => this.filterSelectSearch('AUDIOVISUAL_TYPES'));
@@ -64,11 +64,11 @@ export class FormMainComponent implements OnInit, OnDestroy {
 
     let search = this[`${model.toLowerCase()}FilterCtrl`].value;
     if (!search) {
-      this[model.toLowerCase()].next(staticModels[model].slice());
+      this[`${model.toLowerCase()}$`].next(staticModels[model].slice());
       return;
     } else { search = search.toLowerCase(); }
 
-    this[model.toLowerCase()]
+    this[`${model.toLowerCase()}$`]
     .next(staticModels[model]
     .filter(i => i.label.toLowerCase().indexOf(search) > -1));
   }
