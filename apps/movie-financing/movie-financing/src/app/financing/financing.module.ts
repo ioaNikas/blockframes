fdbe25e3f1cf62333dac85a31bb5d8c17ba7493e
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-06 12:36:59 +0200

    financing(dashboard): layout + stats

--- apps/movie-financing/movie-financing/src/app/financing/financing.module.ts
+++ apps/movie-financing/movie-financing/src/app/financing/financing.module.ts
@@ -6,6 +6,7 @@ import { MatCardModule, MatGridListModule, MatIconModule, MatListModule } from '
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardStatComponent } from './card-stat/card-stat.component';
+import { MaterialLayoutModule } from '@blockframes/ui';
 
 export const routes: Routes = [
   {
@@ -26,7 +27,8 @@ export const routes: Routes = [
     MatGridListModule,
     MatCardModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MaterialLayoutModule
   ],
   providers: [],
   bootstrap: []
