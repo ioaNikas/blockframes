164d73ff7a76c94dfc6d74a54b86058523f33cb5
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-10 17:06:52 +0200

    financing(details): load real movie from id

--- apps/movie-financing/movie-financing/src/app/canne-data/index.ts
+++ apps/movie-financing/movie-financing/src/app/canne-data/index.ts
@@ -1,3 +1,15 @@
 import DATA_ANETTE from './annette';
 
-export default [DATA_ANETTE];
+const DATA = [DATA_ANETTE];
+
+export function getMovie(movieId: string): any {
+  const xs = DATA.filter((x: any) => x.id === movieId);
+
+  if (xs.length === 0) {
+    return null;
+  } else {
+    return xs[0];
+  }
+}
+
+export default DATA;
--- apps/movie-financing/movie-financing/src/app/explorer/details/details.component.ts
+++ apps/movie-financing/movie-financing/src/app/explorer/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { default as CANNE_DATA }  from '../../canne-data';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { getMovie } from '../../canne-data';
 
 @Component({
   selector: 'financing-explorer-details',
@@ -7,21 +8,30 @@ import { default as CANNE_DATA }  from '../../canne-data';
   styleUrls: ['./details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FinancingExplorerDetailsComponent implements OnInit {
+export class FinancingExplorerDetailsComponent implements OnInit, OnDestroy {
   public movie: any;
+  private sub: any;
 
-  constructor() {
-    this.movie = CANNE_DATA[0]; //@todo use @input or router param to select good movie
+  constructor(
+    private router: ActivatedRoute
+  ) {
+    this.sub = router.params.subscribe(params => {
+      this.movie = getMovie(params['id']);
+    });
   }
 
   ngOnInit() {
   }
 
-  public sumItems (items: Array<any>, attr: string) {
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  public sumItems(items: Array<any>, attr: string) {
     let sum = 0;
     items.forEach((item: any) => {
       sum += parseFloat(item[attr]);
-    })
+    });
     return sum;
   }
 }
