d19e9a307f20f533fdc9a81a7e2ea2b5583bb35a
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-13 10:20:39 +0200

    financing(data): todo on data management

--- apps/movie-financing/movie-financing/src/app/canne-data/index.ts
+++ apps/movie-financing/movie-financing/src/app/canne-data/index.ts
@@ -3,5 +3,6 @@ import DATA_ANETTE from './annette';
 export const CANNE_DATA = [DATA_ANETTE];
 
 export function getMovie(movieId: string): any {
+  // TODO: use a well defined data type and throw explicitly when there is no instance.
   return CANNE_DATA.find(x => x.id === movieId) || null;
 }
