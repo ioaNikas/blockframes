e589fde90c6ffa927d901f3ef9418feca6575455
pldespaigne <pl.despaigne@gmail.com>
2019-05-20 14:30:33 +0200

    fix ci build error 3

--- apps/movie-financing/movie-financing/src/app/explorer/compare/compare.component.ts
+++ apps/movie-financing/movie-financing/src/app/explorer/compare/compare.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { AuthQuery, User, AuthService } from '@blockframes/auth';
-import { Observable } from 'rxjs';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'financing-explorer-compare',
@@ -8,30 +6,6 @@ import { Observable } from 'rxjs';
   styleUrls: ['./compare.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FinancingExplorerCompareComponent implements OnInit {
-
-  public user$: Observable<User>;
-  public isBalanceLoading$: Observable<boolean>;
-  public amount: number;
-
-  constructor(private authQuery: AuthQuery, private authService: AuthService) { }
-
-  ngOnInit() {
-    this.user$ = this.authQuery.user$;
-    this.isBalanceLoading$ = this.authQuery.isBalanceLoading$;
-    this.amount = 0.01;
-  }
-
-  handleChange(value) {
-    this.amount = value;
-  }
-
-  refreshBalance() {
-    this.authService.refreshBalance();
-  }
-
-  requestTokens() {
-    this.authService.requestTokens(this.amount);
-  }
-
+export class FinancingExplorerCompareComponent {
+  constructor() {}
 }
