a007e2ce8a56477346e34d27d082a24f2c9dfd34
Laurent Senta <Laurent.Senta@gmail.com>
2019-05-22 11:40:19 +0200

    backend: setup functions in package

--- apps/backend-functions/src/main.ts
+++ apps/backend-functions/src/main.ts
@@ -1,20 +1,8 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
+import * as functions from 'firebase-functions';
 
-import * as express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send(`Welcome to backend-functions!`);
-});
-
-const port = 3333;
-app.listen(port, (err) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(`Listening at http://localhost:${port}`);
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
 });
