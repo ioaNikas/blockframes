ab6fa2e2a17831c6cd0eef7cd2d179953e4545dc
bruce <delorme.bruce.michel@gmail.com>
2019-05-28 18:15:02 +0200

    [WIP] forms refactoring

---
+++ libs/auth/src/lib/forms/signin.form.ts
@@ -0,0 +1,32 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractFormControls } from './abstract.form';
+
+
+export class SigninFormControls extends AbstractFormControls{
+
+  constructor() {
+    super();
+
+    this.controls =  {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      pwd: new FormControl('', this.passwordValidators),
+    };
+  }
+}
+
+export class SigninForm extends FormGroup {
+  protected form : AbstractFormControls;
+
+  constructor(controls? : any, validators?: any ) {
+    const f = new SigninFormControls();
+    super(
+      controls !== undefined ? controls : f.controls,
+      validators !== undefined ? validators : f.validators
+    );
+    this.form = f;
+  }
+
+  public getRules (ruleName: string) {
+    return this.form.rules[ruleName];
+  }
+}
---
+++ libs/auth/src/lib/forms/signup.form.ts
@@ -0,0 +1,33 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RepeatPasswordStateMatcher, AbstractFormControls } from './abstract.form';
+import { SigninFormControls, SigninForm } from './signin.form';
+
+export class SignupFormControls extends SigninFormControls{
+
+  constructor() {
+    super();
+    this.passwordsMatcher = new RepeatPasswordStateMatcher('pwd', 'confirm');
+    this.controls.confirm = new FormControl('', Validators.required);
+    this.validators.push(this.checkPasswords);
+  }
+
+  private checkPasswords(group: FormGroup) {
+    return group.controls.pwd.value === group.controls.confirm.value ? null : { passwordsNotMatching: true }     
+  }
+}
+
+export class SignupForm extends SigninForm {
+
+  constructor(controls? : any, validators?: any ) {
+    const f = new SignupFormControls();
+    super(
+      controls !== undefined ? controls : f.controls,
+      validators !== undefined ? validators : f.validators
+    );
+    this.form = f;
+  }
+
+  public getPasswordMatcher() {
+    return this.form.passwordsMatcher;
+  }
+}
