b9a1ed8345d434ca5a76f22805381792d9f0c414
bruce <delorme.bruce.michel@gmail.com>
2019-06-05 11:42:15 +0200

    #359

--- libs/movie/src/lib/movie/form/form.story.component.ts
+++ libs/movie/src/lib/movie/form/form.story.component.ts
@@ -2,6 +2,7 @@ import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material';
+import { MovieForm } from './movie.form';
 
 @Component({
   selector: 'movie-form-story-section',
@@ -11,28 +12,7 @@ import { MatChipInputEvent } from '@angular/material';
 })
 export class FormStoryComponent {
 
-  @Input() movieForm: FormGroup;
-  @Input() removeFormControl: any;
-  @Input() addFormControl: any;
-  @Input() keywords: FormArray;
-
   public readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  constructor() {}
-
-  public addChip(event: MatChipInputEvent, object: string): void {
-    const input = event.input;
-    const value = event.value;
-
-    // Add keyword
-    if ((value || '').trim()) {
-      this.addFormControl(new FormControl(value.trim()), object);
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-  }
-
+  constructor(public form: MovieForm) {}
 }
