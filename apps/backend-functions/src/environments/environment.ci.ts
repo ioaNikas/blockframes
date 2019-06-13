e1079a7586aa67e9dbe264f61742b88440bbc149
Laurent Senta <Laurent.Senta@gmail.com>
2019-06-13 17:24:30 +0200

    ci: setup tooling
    - ci: prep env during ci build & tweak values
    - ci: NODE_OPTIONS memory to 4GB
    - ci: use ENV=ci

--- apps/backend-functions/src/environments/environment.ci.ts
+++ apps/backend-functions/src/environments/environment.ci.ts
@@ -1,12 +1,4 @@
 /**
- * This environment uses all the configuration defined in the current @env setup,
- * AND loads secrets from the firebase's functions config object.
- *
- * Use this setup (production) when the execution context is within firebase functions.
+ * This environment uses all the configuration defined in the current @env setup.
  */
-import * as functions from 'firebase-functions';
-
-export { backupBucket, relayer, firebase } from '@env';
-
-export const sendgridAPIKey = functions.config().sendgrid.apiKey;
-export const mnemonic = functions.config().relayer.mnemonic;
+export { backupBucket, relayer, firebase, mnemonic, sendgridAPIKey } from '@env';
