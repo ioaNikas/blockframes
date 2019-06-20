af4824eecaf72ab4e1e107b2f80a50c3d390645d
Laurent Senta <Laurent.Senta@gmail.com>
2019-06-20 15:25:07 +0200

    backend-ops: implem & auto run on e2e

--- apps/backend-ops/src/main.ts
+++ apps/backend-ops/src/main.ts
@@ -1,3 +1,10 @@
 import { prepareFirebase } from './firebaseSetup';
 
-prepareFirebase();
+async function run() {
+  console.info("Preparing firebase...")
+  await prepareFirebase();
+  console.info("Firebase ready for testing!")
+  process.exit(0)
+}
+
+run()
