720700b228382fb0ae4deae35469f16f1e210651
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-06-20 09:44:41 +0200

    Password component uses minlength validator error

--- libs/ui/src/lib/form/inputs/password/password.component.ts
+++ libs/ui/src/lib/form/inputs/password/password.component.ts
@@ -15,6 +15,18 @@ export class PasswordComponent  {
   @Input() name = 'password';
   @Input() placeholder = 'Password';
   @Input() matcher: ErrorStateMatcher;
-  
+
   constructor() {}
+
+  public get control() {
+    return this.form.get(this.name);
+  }
+
+  public get minLength(): { requiredLength: number, actualLength: number } {
+    return this.control.errors.minlength;
+  }
+
+  public get maxLength(): { requiredLength: number, actualLength: number } {
+    return this.control.errors.maxlength;
+  }
 }
