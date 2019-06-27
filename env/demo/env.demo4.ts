a39e5e1ce6d1c2e253a800ffa1e6b20eff54b85d
Laurent Senta <Laurent.Senta@gmail.com>
2019-06-27 15:45:24 +0200

    update demo envs

--- env/demo/env.demo4.ts
+++ env/demo/env.demo4.ts
@@ -1,6 +1,14 @@
 export const production = true;
 export const hmr = false;
 
+export const persistenceSettings = {
+  // TODO : change to synchronizeTabs when available (angularFire 5.2)
+  experimentalTabSynchronization: true
+};
+
+// Firebase
+// ========
+
 export const firebase = {
   apiKey: 'AIzaSyB8Ch4AX-SsVbl76goSlkfS9FHh6FkT5MY',
   authDomain: 'blockframes-demo-4.firebaseapp.com',
@@ -10,15 +18,31 @@ export const firebase = {
   messagingSenderId: '549985951125'
 };
 
-// export const network = 'homestead';
-// export const baseEnsDomain = 'blockframes.eth';
+// Ethereum
+// ========
 
-// DEMO VERSION: We use our staging setup to run the current production.
 export const network = 'goerli';
+export const mnemonic = ''; // defined in functions.config, see backend-functions/environments
 export const baseEnsDomain = 'blockframes.test';
+export const factoryContract = 'factory2.eth';
 
+// TODO : change the address
 export const contracts = {
   ipHash: '0x6f77765b18deac65dc55c3a38a112c9583e25185',
   testErc1077: '0x758011e12E57a81f93D1e59AdF8867463349A54d',
   ensResolver: '0xc1EA41786094D1fBE5aded033B5370d51F7a3F96'
 };
+
+export const relayer = {
+  registryAddress: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+  resolverAddress: '0xc1EA41786094D1fBE5aded033B5370d51F7a3F96',
+  network,
+  baseEnsDomain,
+  factoryContract
+};
+
+// Functions
+// =========
+
+export const backupBucket = 'backups';
+export const sendgridAPIKey = ''; // defined in functions.config, see backend-functions/environments
