1b01f485183e7272d145c204c43bd2e73d914ddf
Laurent Senta <Laurent.Senta@gmail.com>
2019-06-27 15:45:59 +0200

    deploy-demos: update script

--- deploy-demos.sh
+++ deploy-demos.sh
@@ -1,25 +1,39 @@
 #! /usr/bin/env bash
-
 set -x
 set -e
 
+export NODE_OPTIONS="--max_old_space_size=8192"
+
+git checkout demo
+TAG="demo-$(date +'%Y%m%d%H%M')"
+
 for i in {1..5}; do
-    echo $i;
+  echo $i
+  export ENV=prod
 
-    cp ./env/demo/env.demo${i}.ts ./env/env.ts
-    cp ./env/demo/env.demo${i}.ts ./env/env.prod.ts
+  cp ./env/demo/env.demo${i}.ts ./env/env.ts
+  cp ./env/demo/env.demo${i}.ts ./env/env.prod.ts
 
-    npm run build:all
+  ENV=prod npx ng lint
+  ENV=prod npm run build:all
 
-    firebase use demo${i}
-    ./secrets.sh demo${i}
+  cp ./package.json dist/apps/backend-functions/
 
-    # we deploy everything but functions, they tend to crash
-    firebase deploy --except functions
+  firebase use demo${i}
+  ./secrets.sh demo${i}
+  ./ops/deploy_secrets.sh
 
-    # we deploy the functions and retry until it worked
-    until firebase deploy --only functions || true; do
-        echo retry;
-        sleep 5;
-    done
+  # we deploy everything but functions, they tend to crash
+  firebase deploy --except functions
+
+  # we deploy the functions and retry until it worked
+  until firebase deploy --only functions; do
+    echo retry
+    sleep 5
+  done
+
+  scp -r dist/apps/* blockframes:~/www/www-data/demo${i}/
 done
+
+git tag "${TAG}"
+git push origin "${TAG}"
