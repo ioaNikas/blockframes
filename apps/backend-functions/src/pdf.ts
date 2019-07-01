4a19eec57de1363ff374cf2c2eb9184f41c19be7
Laurent Senta <Laurent.Senta@gmail.com>
2019-07-01 17:49:34 +0200

    backend(pdf): refactor & document

--- apps/backend-functions/src/pdf.ts
+++ apps/backend-functions/src/pdf.ts
@@ -5,6 +5,10 @@ import { groupBy, sortBy } from 'lodash';
 
 const PdfPrinter = require('pdfmake');
 
+// Types
+// =====
+
+// TODO: extract and sync with the rest of the backend codebase
 interface StakeHolder {
   name: string;
   organization: string;
@@ -30,6 +34,8 @@ interface DeliveryContent {
   materials: Material[];
 }
 
+// Constants for styles & fonts
+// ============================
 const FONTS = {
   Helvetica: {
     normal: 'Helvetica',
@@ -39,20 +45,24 @@ const FONTS = {
   }
 };
 
-const HEADER = 'header';
-const SUBHEADER = 'subheader';
-const DESCRIPTION = 'description';
-const BOLD = 'bold';
+const STYLE_HEADER = 'header';
+const STYLE_SUBHEADER = 'subheader';
+const STYLE_DESCRIPTION = 'description';
+const STYLE_BOLD = 'bold';
+const STYLE_TABLE_MATERIALS = 'styleTableMaterials';
+
+// Helpers
+// -------
 
 const withStyle = (text: string, style: string): any => ({
   text,
   style
 });
 
-const header = (text: string) => withStyle(text, HEADER);
-const subHeader = (text: string) => withStyle(text, SUBHEADER);
-const description = (text: string) => withStyle(text, DESCRIPTION);
-const bold = (text: string) => withStyle(text, BOLD);
+const header = (text: string) => withStyle(text, STYLE_HEADER);
+const subHeader = (text: string) => withStyle(text, STYLE_SUBHEADER);
+const description = (text: string) => withStyle(text, STYLE_DESCRIPTION);
+const bold = (text: string) => withStyle(text, STYLE_BOLD);
 
 const center = (content: string | { [k: string]: any }): any => {
   if (typeof content === typeof 'string') {
@@ -63,91 +73,120 @@ const center = (content: string | { [k: string]: any }): any => {
   }
 };
 
-export function buildDeliveryPDF({ txID, stakeholders, materials, steps }: DeliveryContent) {
-  // We store keys first to make sure order is preserved along the way
-  const stakeholdersId = Object.keys(stakeholders);
-  const stakeholdersHeader: any = stakeholdersId.map((id: string) => {
+// PDF Generation
+// ==============
+
+// These functions take our blockframes model and generate data the pdf rendering.
+
+function rowStakeholders(
+  stakeholdersIds: string[],
+  stakeholders: { [id: string]: StakeHolder }
+): any {
+  const columns: any = stakeholdersIds.map((id: string) => {
     const stakeholder = stakeholders[id];
     return [subHeader(stakeholder.name), description(stakeholder.organization)];
   });
+  return [
+    header('Stakeholders'),
+    {
+      alignment: 'center',
+      columns
+    }
+  ];
+}
 
+function rowMaterials(materials: Material[], steps: { [id: string]: Step }): any {
+  // NOTE: pdfmake side-effect over the data provided, we can reuse the same objects
+  // multiple time, we have to keep this variable definition INSIDE the forEach.
+  const tableHeader = [bold('material'), center(bold('step'))];
+
+  const tableBody = materials.map(material => [
+    [bold(material.value), description(material.description)],
+    center({
+      text: material.stepId ? steps[material.stepId].name : '',
+      margin: [0, 4, 0, 0]
+    })
+  ]);
+
+  return {
+    style: STYLE_TABLE_MATERIALS,
+    table: {
+      headerRows: 1,
+      widths: ['80%', '20%'],
+      body: [[...tableHeader], ...tableBody]
+    }
+  };
+}
+
+function rowMaterialsPerCategory(materials: Material[], steps: { [id: string]: Step }): any {
   const materialsPerCategory = groupBy(materials, (material: Material) => material.category);
   const categories = sortBy(Object.keys(materialsPerCategory));
 
-  const tables: any = [];
+  const tables: any[] = [];
+
   categories.forEach(category => {
-    // NOTE: pdfmake side-effect over the data provided, we can reuse the same objects
-    // multiple time, we have to keep this variable definition INSIDE the forEach.
-    const tableHeader = [bold('material'), center(bold('step'))];
-
     tables.push(subHeader(category));
-
-    const tableBody = materialsPerCategory[category].map(material => [
-      [bold(material.value), description(material.description)],
-      center({
-        text: material.stepId ? steps[material.stepId].name : '',
-        margin: [0, 4, 0, 0]
-      })
-    ]);
-
-    tables.push({
-      style: 'materialsTable',
-      table: {
-        headerRows: 1,
-        widths: ['80%', '20%'],
-        body: [[...tableHeader], ...tableBody]
-      }
-    });
+    tables.push(rowMaterials(materialsPerCategory[category], steps));
   });
 
-  const signatures = {
-    alignment: 'center',
-    columns: stakeholdersId.map((id: string) => {
-      const stakeholder = stakeholders[id];
-      const tx = txID[id];
+  return [header('Materials'), ...tables];
+}
 
-      return [bold(stakeholder.name), { qr: tx, fit: '100' }];
-    })
-  };
+function rowSignatures(
+  stakeholdersIds: string[],
+  stakeholders: { [id: string]: StakeHolder },
+  txID: { [stkID: string]: string }
+): any {
+  const columns = stakeholdersIds.map((id: string) => {
+    const stakeholder = stakeholders[id];
+    const tx = txID[id];
 
-  //const materials: any = materials.map((x: any) => ['A', x.name, '20 July 2019']);
+    return [bold(stakeholder.name), { qr: tx, fit: '100' }];
+  });
+
+  return [
+    header('Signatures'),
+    {
+      alignment: 'center',
+      columns
+    }
+  ];
+}
+
+export function buildDeliveryPDF({ txID, stakeholders, materials, steps }: DeliveryContent) {
+  // We store keys first to make sure order is preserved along the way
+  const stakeholdersIds = Object.keys(stakeholders);
 
   const docDefinition = {
+    content: [
+      ...rowStakeholders(stakeholdersIds, stakeholders),
+      ...rowMaterialsPerCategory(materials, steps),
+      ...rowSignatures(stakeholdersIds, stakeholders, txID)
+    ],
+    defaultStyle: {
+      font: 'Helvetica'
+    },
     styles: {
-      materialsTable: {
+      [STYLE_TABLE_MATERIALS]: {
         margin: [5, 5, 5, 5]
       },
-      [HEADER]: {
+      [STYLE_HEADER]: {
         fontSize: 18,
         bold: true,
         margin: [5, 20, 5, 5]
       },
-      [SUBHEADER]: {
+      [STYLE_SUBHEADER]: {
         fontSize: 14,
         bold: true,
         margin: [5, 10, 5, 5]
       },
-      [DESCRIPTION]: {
+      [STYLE_DESCRIPTION]: {
         fontSize: 10,
         italics: true
       },
-      [BOLD]: {
+      [STYLE_BOLD]: {
         bold: true
       }
-    },
-    content: [
-      header('Stakeholders'),
-      {
-        alignment: 'center',
-        columns: stakeholdersHeader
-      },
-      header('Materials'),
-      ...tables,
-      header('Signatures'),
-      signatures
-    ],
-    defaultStyle: {
-      font: 'Helvetica'
     }
   };
   const printer = new PdfPrinter(FONTS);
