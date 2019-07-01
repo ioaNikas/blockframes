152d5c4ff11b94afb7525cb1d207a3732cd08d43
Laurent Senta <Laurent.Senta@gmail.com>
2019-07-01 17:08:28 +0200

    backend(pdf): complete data set for test generation

--- apps/backend-functions/src/local/testPDFExport.ts
+++ apps/backend-functions/src/local/testPDFExport.ts
@@ -2,18 +2,55 @@ import { buildDeliveryPDF } from '../pdf';
 import * as fs from 'fs';
 
 const testData = {
-  txID: ['0x9e91a0b95412093e639189a05a2dbf68e16c3062001e673826616222baffcac8'],
-  stakeholders: [
-    { name: 'John Doe', organization: 'LogicalPicture' },
-    { name: 'Tomme Hardy', organization: '20Th Century Fox' },
-    { name: 'Francis Munster', organization: 'Disney' }
-  ],
+  txID: {
+    id1: '0x9e91a0b95412093e639189a05a2dbf68e16c3062001e673826616222baffcac8',
+    id2: '0x8e91a0b95412093e639189a05a2dbf68e16c3062001e673826616222baffcac8',
+    id3: '0x7e91a0b95412093e639189a05a2dbf68e16c3062001e673826616222baffcac8'
+  },
+  stakeholders: {
+    id1: { name: 'John Doe', organization: 'LogicalPicture' },
+    id2: { name: 'Tomme Hardy', organization: '20Th Century Fox' },
+    id3: { name: 'Francis Munster', organization: 'Disney' }
+  },
+  steps: {
+    i36vwU1eVdlNObEafOre: { name: 'A', date: new Date() },
+    P8uVlb5CD0i6NU8fegAf: { name: 'B', date: new Date() }
+  },
   materials: [
     {
-      name: 'Version Originale'
+      value: 'My Second Material',
+      category: 'Some Category',
+      id: '0DL3qyDacTcsyIQUCU0R',
+      description: 'My Second Material Description',
+      stepId: 'i36vwU1eVdlNObEafOre'
     },
     {
-      name: 'Subtitles FR'
+      value: 'My Third Material',
+      category: 'Another Category',
+      id: 'Ci3RPg5qLLNTo1e5n7L0',
+      description: 'My Third Material Description',
+      stepId: 'i36vwU1eVdlNObEafOre'
+    },
+    {
+      description: 'Yet Another Material With a Description',
+      stepId: 'P8uVlb5CD0i6NU8fegAf',
+      value: 'Yet Another Material',
+      category: 'Another Category',
+      id: 'OtTFuS6Lq3MdjfGicU8v'
+    },
+    {
+      description: 'My Material No Step Description',
+      stepId: '',
+      value: 'My Material No Step',
+      category: 'Another Category',
+      id: 'byVmOtNzxPNJZv8qs1OX'
+    },
+    {
+      description: 'My Material Description',
+      stepId: 'i36vwU1eVdlNObEafOre',
+      value: 'My Material',
+      category: 'Some Category',
+      id: 'jYpavkTXisbfGfQT873I'
     }
   ]
 };
--- apps/backend-functions/src/pdf.ts
+++ apps/backend-functions/src/pdf.ts
@@ -1,9 +1,34 @@
 /**
  * Deals with PDF exports for various parts of the application.
  */
+import { groupBy, sortBy } from 'lodash';
+
 const PdfPrinter = require('pdfmake');
 
-type DeliveryContent = any;
+interface StakeHolder {
+  name: string;
+  organization: string;
+}
+
+interface Step {
+  name: string;
+  date: Date;
+}
+
+interface Material {
+  value: string;
+  category: string;
+  description: string;
+  id: string;
+  stepId: string;
+}
+
+interface DeliveryContent {
+  txID: { [stakeholderID: string]: string };
+  stakeholders: { [stakeholderID: string]: StakeHolder };
+  steps: { [id: string]: Step };
+  materials: Material[];
+}
 
 const FONTS = {
   Helvetica: {
@@ -14,43 +39,112 @@ const FONTS = {
   }
 };
 
-export function buildDeliveryPDF(content: DeliveryContent) {
-  let header: any = content.stakeholders.map((x: any) => [
-    { text: x.name },
-    { text: x.organization }
-  ]);
+const HEADER = 'header';
+const SUBHEADER = 'subheader';
+const DESCRIPTION = 'description';
+const BOLD = 'bold';
 
-  if (content.txID) {
-    header = [{ qr: content.txID }, ...header];
+const withStyle = (text: string, style: string): any => ({
+  text,
+  style
+});
+
+const header = (text: string) => withStyle(text, HEADER);
+const subHeader = (text: string) => withStyle(text, SUBHEADER);
+const description = (text: string) => withStyle(text, DESCRIPTION);
+const bold = (text: string) => withStyle(text, BOLD);
+
+const center = (content: string | { [k: string]: any }): any => {
+  if (typeof content === typeof 'string') {
+    return { text: content, alignment: 'center' };
+  } else {
+    // @ts-ignore: TODO: debug this switch
+    return { ...content, alignment: 'center' };
   }
+};
 
-  const materials: any = content.materials.map((x: any) => ['A', x.name, '20 July 2019']);
+export function buildDeliveryPDF({ txID, stakeholders, materials, steps }: DeliveryContent) {
+  // We store keys first to make sure order is preserved along the way
+  const stakeholdersId = Object.keys(stakeholders);
+  const stakeholdersHeader: any = stakeholdersId.map((id: string) => {
+    const stakeholder = stakeholders[id];
+    return [subHeader(stakeholder.name), description(stakeholder.organization)];
+  });
+
+  const materialsPerCategory = groupBy(materials, (material: Material) => material.category);
+  const categories = sortBy(Object.keys(materialsPerCategory));
+
+  const tables: any = [];
+  categories.forEach(category => {
+    // NOTE: pdfmake side-effect over the data provided, we can reuse the same objects
+    // multiple time, we have to keep this variable definition INSIDE the forEach.
+    const tableHeader = [bold('material'), center(bold('step'))];
+
+    tables.push(subHeader(category));
+
+    const tableBody = materialsPerCategory[category].map(material => [
+      [bold(material.value), description(material.description)],
+      center({
+        text: material.stepId ? steps[material.stepId].name : '',
+        margin: [0, 4, 0, 0]
+      })
+    ]);
+
+    tables.push({
+      style: 'materialsTable',
+      table: {
+        headerRows: 1,
+        widths: ['80%', '20%'],
+        body: [[...tableHeader], ...tableBody]
+      }
+    });
+  });
+
+  const signatures = {
+    alignment: 'center',
+    columns: stakeholdersId.map((id: string) => {
+      const stakeholder = stakeholders[id];
+      const tx = txID[id];
+
+      return [bold(stakeholder.name), { qr: tx, fit: '100' }];
+    })
+  };
+
+  //const materials: any = materials.map((x: any) => ['A', x.name, '20 July 2019']);
 
   const docDefinition = {
     styles: {
       materialsTable: {
         margin: [5, 5, 5, 5]
       },
-      header: {
+      [HEADER]: {
         fontSize: 18,
+        bold: true,
+        margin: [5, 20, 5, 5]
+      },
+      [SUBHEADER]: {
+        fontSize: 14,
+        bold: true,
+        margin: [5, 10, 5, 5]
+      },
+      [DESCRIPTION]: {
+        fontSize: 10,
+        italics: true
+      },
+      [BOLD]: {
         bold: true
       }
     },
     content: [
-      { text: 'Stakeholders', style: 'header' },
+      header('Stakeholders'),
       {
-        alignment: 'justify',
-        columns: header
+        alignment: 'center',
+        columns: stakeholdersHeader
       },
-      { text: 'Materials', style: 'header' },
-      {
-        style: 'materialsTable',
-        table: {
-          headerRows: 1,
-          widths: [40, '*', 140],
-          body: [['A', 'Name', 'Due Date'], ...materials]
-        }
-      }
+      header('Materials'),
+      ...tables,
+      header('Signatures'),
+      signatures
     ],
     defaultStyle: {
       font: 'Helvetica'
