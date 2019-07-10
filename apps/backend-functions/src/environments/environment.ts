a650b8286cc1067893f471f6eb35c194efc93d61
Laurent Senta <Laurent.Senta@gmail.com>
2019-07-10 17:55:23 +0200

    backend(env): get rid of firebase.config in local env

--- apps/backend-functions/src/environments/environment.ts
+++ apps/backend-functions/src/environments/environment.ts
@@ -5,9 +5,4 @@
  * Use this setup (non-production) when the execution context is outside firebase
  * functions.
  */
-import * as functions from 'firebase-functions';
-
-export { factoryContract, backupBucket, relayer, appUrl } from '@env';
-
-export const sendgridAPIKey = functions.config().sendgrid.apiKey;
-export const mnemonic = functions.config().relayer.mnemonic;
+export { factoryContract, backupBucket, relayer, appUrl, sendgridAPIKey, mnemonic } from '@env';
