fa015b63fffe1e240e1acf6bf632099a9d1ef38c
Max Fritz <max.j.f@gmx.de>
2019-07-12 12:01:33 +0200

    fixed ci build and validMnemonic validator

--- libs/utils/src/lib/form/validators/validators.ts
+++ libs/utils/src/lib/form/validators/validators.ts
@@ -1,44 +1,41 @@
-import { Validators, FormGroup, ValidatorFn, FormControl, AbstractControl, ValidationErrors } from '@angular/forms';
+import {
+  Validators,
+  FormGroup,
+  ValidatorFn,
+  FormControl,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
 import { ethers } from 'ethers';
 
-export const emailValidators = [
-  Validators.required,
-  Validators.email
-]
+export const emailValidators = [Validators.required, Validators.email];
 
-export const stringValidators = [
+export const stringValidators = [];
 
-];
+export const urlValidators = [Validators.pattern('^(http|https)://[^ "]+$')];
 
-export const urlValidators = [
-  Validators.pattern('^(http|https):\/\/[^ "]+$')
-];
+export const yearValidators = [Validators.pattern('^[1-2][0-9]{3}$')];
 
-export const yearValidators = [
-  Validators.pattern("^[1-2][0-9]{3}$"),
-];
+export const ethereumPublicAddressValidators = [Validators.pattern('^0x[0-9a-fA-F]{40}$')];
 
-export const ethereumPublicAddressValidators = [
-  Validators.pattern('^0x[0-9a-fA-F]{40}$'),
-];
-
-export const ethereumPrivateAddressValidators = [
-  Validators.pattern('^[0-9a-fA-F]{64}$'),
-];
+export const ethereumPrivateAddressValidators = [Validators.pattern('^[0-9a-fA-F]{64}$')];
 
 /** Require password and password confirm inputs to be the same */
-export function confirmPasswords(password: string = 'password', confirm: string = 'confirm'): ValidatorFn {
+export function confirmPasswords(
+  password: string = 'password',
+  confirm: string = 'confirm'
+): ValidatorFn {
   return (group: FormGroup): { [key: string]: boolean } | null => {
     return group.controls[password].value === group.controls[confirm].value
       ? null
       : { passwordsNotMatching: true };
   };
-};
+}
 
 /** Require **either** mnemonic **or** private key **but not both** */
 export function requireMnemonicXorPrivateKey(control: FormControl) {
   const { mnemonic, privateKey } = control.value;
-  return (!!mnemonic !== !!privateKey) ? null : { bothEmpty: true }; // logical XOR
+  return !!mnemonic !== !!privateKey ? null : { bothEmpty: true }; // logical XOR
 }
 
 /** Checks if the inputted mnemonic is a valid mnemonic */
@@ -50,5 +47,5 @@ export function validMnemonic(control: AbstractControl): ValidationErrors | null
   }
   // Use ethersjs build in function to check for a correct Mnemonic
   const isValidMnemonic = ethers.utils.HDNode.isValidMnemonic(control.value);
-  return isValidMnemonic ? { mnemonic: true } : null;
+  return isValidMnemonic ? null : { mnemonic: true };
 }
