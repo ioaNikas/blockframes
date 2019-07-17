d7f50e037b0b251bf0af6b8c0af7a6cd076ab8e3
hugo-hontans <hugohontans@gmail.com>
2019-07-17 15:03:19 +0200

    implements  organization-find logic

--- libs/organization/src/lib/pages/organization-find/organization-find.component.ts
+++ libs/organization/src/lib/pages/organization-find/organization-find.component.ts
@@ -1,8 +1,11 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { startWith, map, filter } from 'rxjs/operators';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { startWith, map, filter, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Organization } from '../../+state';
+import firebase from 'firebase';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'organization-find',
@@ -11,32 +14,64 @@ import { Router } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class OrganizationFindComponent implements OnInit {
-  public addOrganizationForm = new FormGroup({
-    org: new FormControl('')
-  });
-  orgOptions: Observable<string[]>;
-  options: string[] = ['organization name', 'super org', 'org on fire'];
+export class OrganizationFindComponent implements OnInit, OnDestroy {
+  public addOrganizationForm: FormGroup;
+  public orgOptions: Organization[];
+  private selectedOrganization: Partial<Organization>;
+  private destroyed$ = new Subject();
 
-  constructor(private router: Router) {}
+  constructor(
+    private builder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
-    this.orgOptions = this.addOrganizationForm.valueChanges
-      .pipe(
-        startWith(''),
-        filter(value => !!value),
-        map(value => this._filter(value.org))
-      );
+    this.orgOptions = [];
+    this.addOrganizationForm = this.builder.group({
+      org: null
+    });
+    this.onChange();
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  public selectOrganization(organization: Partial<Organization>) {
+    this.selectedOrganization = organization;
   }
 
   public addOrganization() {
-    // Create a new invitation
-    console.log(this.addOrganizationForm.value);
-    this.router.navigate(['layout/congratulation']);
+    if(this.selectedOrganization) {
+      // TODO: create a new invitation
+      console.log(this.selectedOrganization);
+    }
+    else {
+      this.snackBar.open('Please select an organization', 'close', { duration: 2000 });
+    }
+  }
+
+  public displayFn(organization?: Organization): string | undefined {
+    return organization ? organization.name : undefined;
+  }
+
+  private async listOrgsByName(prefix: string): Promise<Organization[]> {
+    const call = firebase.functions().httpsCallable('findOrgByName');
+    return call({ prefix }).then(matchingOrgs => matchingOrgs.data);
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.unsubscribe();
+  }
+
+  private async onChange() {
+    this.addOrganizationForm.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.destroyed$)
+      ).subscribe(typingOrgName => {
+        this.listOrgsByName(typingOrgName.org).then(matchingOrgs => {
+          // TODO: use an observable => ISSUE#608
+          this.orgOptions = matchingOrgs;
+        });
+    });
   }
 }
