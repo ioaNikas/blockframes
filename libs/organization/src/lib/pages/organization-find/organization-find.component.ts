e42abb84f91efff15d7519fe4e472bf39988ede2
hugo-hontans <hugohontans@gmail.com>
2019-07-18 11:55:31 +0200

    clean code

--- libs/organization/src/lib/pages/organization-find/organization-find.component.ts
+++ libs/organization/src/lib/pages/organization-find/organization-find.component.ts
@@ -1,9 +1,9 @@
 import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
-import { startWith, map, filter, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { Organization, OrganizationService } from '../../+state';
+import { Organization } from '../../+state';
 import firebase from 'firebase';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { InvitationService } from '@blockframes/notification';
@@ -33,6 +33,7 @@ export class OrganizationFindComponent implements OnInit, OnDestroy {
     this.addOrganizationForm = this.builder.group({
       org: null
     });
+    // TODO: issue#577 new search function
     this.onChange();
   }
 
@@ -54,18 +55,10 @@ export class OrganizationFindComponent implements OnInit, OnDestroy {
     }
   }
 
-  public displayFn(organization?: Organization): string | undefined {
-    return organization ? organization.name : undefined;
-  }
-
+  // TODO: issue#577 new search function
   private async listOrgsByName(prefix: string): Promise<Organization[]> {
     const call = firebase.functions().httpsCallable('findOrgByName');
     return call({ prefix }).then(matchingOrgs => matchingOrgs.data);
-  }
-
-  ngOnDestroy() {
-    this.destroyed$.next();
-    this.destroyed$.unsubscribe();
   }
 
   private async onChange() {
@@ -76,9 +69,13 @@ export class OrganizationFindComponent implements OnInit, OnDestroy {
         takeUntil(this.destroyed$)
       ).subscribe(typingOrgName => {
         this.listOrgsByName(typingOrgName.org).then(matchingOrgs => {
-          // TODO: use an observable => ISSUE#608
           this.orgOptions = matchingOrgs;
         });
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.unsubscribe();
   }
 }
