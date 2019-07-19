52c61ea090bcb61d9a4cf94da5bc546dca073f31
Laurent Senta <Laurent.Senta@gmail.com>
2019-07-19 10:56:03 +0200

    backend(algolia): dev mode with no config

--- apps/backend-functions/src/algolia.ts
+++ apps/backend-functions/src/algolia.ts
@@ -2,15 +2,24 @@ import algoliasearch from 'algoliasearch';
 
 import { algoliaAdminKey, algoliaId } from './environments/environment';
 
-const client = algoliasearch(algoliaId, algoliaAdminKey);
-const INDEX_NAME_ORGANIZATIONS = 'orgs';
-
-const indexOrganizationsBuilder = () => client.initIndex(INDEX_NAME_ORGANIZATIONS);
+const indexOrganizationsBuilder = () => {
+  const client = algoliasearch(algoliaId, algoliaAdminKey);
+  const INDEX_NAME_ORGANIZATIONS = 'orgs';
+  return client.initIndex(INDEX_NAME_ORGANIZATIONS);
+};
 
 export function storeSearchableOrg(orgId: string, name: string): Promise<any> {
+  if (algoliaId === "") {
+    console.warn("No algolia id set, assuming dev config: skipping")
+  }
+
   return indexOrganizationsBuilder().saveObject({objectID: orgId, name});
 }
 
 export function deleteSearchableOrg(orgId: string): Promise<any> {
+  if (algoliaId === "") {
+    console.warn("No algolia id set, assuming dev config: skipping")
+  }
+
   return indexOrganizationsBuilder().deleteObject(orgId);
 }
