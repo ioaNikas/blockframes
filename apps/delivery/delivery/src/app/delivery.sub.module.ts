be09029c2aa37baab71667ca1d390fe918c52dd0
Yohann <ioa.nikas@gmail.com>
2019-07-26 11:59:04 +0200

    Created a route builder function

--- apps/delivery/delivery/src/app/delivery.sub.module.ts
+++ apps/delivery/delivery/src/app/delivery.sub.module.ts
@@ -1,50 +1,9 @@
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { HttpClientModule } from '@angular/common/http';
-
-// Akita
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { environment } from '../environments/environment';
-
-// Angular Fire
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireFunctionsModule } from '@angular/fire/functions';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-
-// Libraries
-import { AuthModule } from '@blockframes/auth';
-import { UiFormModule, UploadModule, ToolbarModule } from '@blockframes/ui';
-import { MovieModule } from '@blockframes/movie';
-import { OrganizationModule, NoOrganizationModule } from '@blockframes/organization';
-import { ProfileModule } from '@blockframes/account';
-import { AccountModule } from '@blockframes/account';
-import { WalletModule } from '@blockframes/ethers';
-import { KeyManagerModule } from '@blockframes/ethers';
-import { NotificationModule } from 'libs/notification/notification.module';
-
-// Material
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatRippleModule } from '@angular/material/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieActiveGuard } from '@blockframes/movie';
 
-export const routes: Routes = [
+export const deliveryRoutes: Routes = [
   { path: '',
     redirectTo: 'movie',
     pathMatch: 'full'
@@ -68,53 +27,8 @@ export const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    // Angular
-    FlexLayoutModule,
-    HttpClientModule,
-
-    // Material
-    MatSnackBarModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule,
-    MatRippleModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatChipsModule,
-    MatCheckboxModule,
-    MatProgressSpinnerModule,
-    MatMenuModule,
-    MatBadgeModule,
-
-    // Libraries
-    AuthModule,
-    UploadModule,
-    UiFormModule,
-    OrganizationModule,
-    ToolbarModule,
-    MovieModule,
-    AccountModule,
-    ProfileModule,
-    WalletModule,
-    KeyManagerModule,
-    NotificationModule,
-    NoOrganizationModule,
-
-    // Firebase
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule.enablePersistence(environment.persistenceSettings),
-    AngularFireFunctionsModule,
-
-    // Akita
-    AkitaNgRouterStoreModule.forRoot(),
-    environment.production ? [] : [AkitaNgDevtools.forRoot()],
-
-    RouterModule.forChild(routes)
+    CommonModule,
+    RouterModule.forChild(deliveryRoutes)
   ],
   providers: [],
 })
--- apps/main/main/src/app/app-routing-module.ts
+++ apps/main/main/src/app/app-routing-module.ts
@@ -1,6 +1,6 @@
 // Angular
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 
 // Components
 import { LayoutComponent } from './layout/layout.component';
@@ -10,75 +10,90 @@ import { AuthGuard } from '@blockframes/auth';
 import { OrganizationGuard, PermissionsGuard } from '@blockframes/organization';
 import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
+/**
+ * Returns the base routes (with basic guards like auth/organization/permissions)
+ * and takes children routes as an argument.
+ * @param children must be an array of Route objects. Usually, we use app specific routes for this param.
+ */
+export const routeFactory = (...children: Route[]) => {
+  return [
+    {
+      path: '',
+      redirectTo: 'auth',
+      pathMatch: 'full'
+    },
+    {
+      path: 'auth',
+      loadChildren: () => import('@blockframes/auth').then(m => m.AuthModule)
+    },
+    {
+      path: 'layout',
+      component: LayoutComponent,
+      canActivate: [AuthGuard],
+      // canDeactivate: [AuthGuard], // TODO: Fix the bug we're getting when trying to logout => ISSUE#670
+      children: [
+        {
+          path: '',
+          redirectTo: 'o',
+          pathMatch: 'full'
+        },
+        {
+          path: 'organization',
+          loadChildren: () => import('@blockframes/organization').then(m => m.NoOrganizationModule)
+        },
+        {
+          path: 'o',
+          canActivate: [PermissionsGuard, OrganizationGuard],
+          canDeactivate: [PermissionsGuard, OrganizationGuard],
+          children: [
+            ...children,
+            {
+              path: 'account',
+              loadChildren: () => import('@blockframes/account').then(m => m.AccountModule)
+            },
+            {
+              path: 'organization',
+              loadChildren: () =>
+                import('@blockframes/organization').then(m => m.OrganizationModule)
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: 'not-found',
+      loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
+    },
+    {
+      path: '**',
+      loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
+    }
+  ];
+};
+
+/**
+ * Main app specific routes. Use it to display main app home
+ * and lazy-load applications submodules.
+ */
+export const routes = routeFactory(
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'auth',
-    loadChildren: () => import('@blockframes/auth').then(m => m.AuthModule)
+    path: 'home',
+    component: HomeComponent
   },
   {
-    path: 'layout',
-    component: LayoutComponent,
-    canActivate: [AuthGuard],
-    // canDeactivate: [AuthGuard], // TODO: Fix the bug we're getting when trying to logout => ISSUE#670
-    children: [
-      {
-        path: '',
-        redirectTo: 'o',
-        pathMatch: 'full'
-      },
-      {
-        path: 'organization',
-        loadChildren: () => import('@blockframes/organization').then(m => m.NoOrganizationModule)
-      },
-      {
-        path: 'o',
-        canActivate: [PermissionsGuard, OrganizationGuard],
-        canDeactivate: [PermissionsGuard, OrganizationGuard],
-        children: [
-          {
-            path: '',
-            redirectTo: 'home',
-            pathMatch: 'full'
-          },
-          {
-            path: 'home',
-            component: HomeComponent
-          },
-          {
-            path: 'account',
-            loadChildren: () => import('@blockframes/account').then(m => m.AccountModule)
-          },
-          {
-            path: 'organization',
-            loadChildren: () => import('@blockframes/organization').then(m => m.OrganizationModule)
-          },
-          {
-            path: 'delivery',
-            loadChildren: () => import('@blockframes/delivery').then(m => m.DeliverySubModule)
-          },
-          {
-            path: 'movie-financing',
-            loadChildren: () =>
-              import('@blockframes/movie-financing').then(m => m.MovieFinancingAppModule)
-          }
-        ]
-      }
-    ]
+    path: 'delivery',
+    loadChildren: () => import('@blockframes/delivery').then(m => m.DeliverySubModule)
   },
   {
-    path: 'not-found',
-    loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
-  },
-  {
-    path: '**',
-    loadChildren: () => import('@blockframes/ui').then(m => m.ErrorNotFoundModule)
+    path: 'movie-financing',
+    loadChildren: () => import('@blockframes/movie-financing').then(m => m.MovieFinancingAppModule)
   }
-];
+);
 
 @NgModule({
   imports: [
