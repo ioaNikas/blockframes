3c1849797a2efe69f9db4a6c2b62d435f3028b27
bruce <delorme.bruce.michel@gmail.com>
2019-08-01 12:27:02 +0200

    simple change on context menu

--- apps/catalog/catalog/src/app/layout/context-menu.ts
+++ apps/catalog/catalog/src/app/layout/context-menu.ts
@@ -2,7 +2,7 @@ export const CONTEXT_MENU = [
   {
     route: 'default',
     items: [
-      { name: 'import', path: '/layout/o/home/import' }, // temp until good place for this route is found
+      { name: 'Upload avails & films doc', path: '/layout/o/home/import' }, // @todo #643 temp until good place for this route is found
     ]
   }
 ];
