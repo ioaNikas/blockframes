b5e709fbfe5bd0cb7cc4a3727ebd0b604dd231a7
Laurent Senta <Laurent.Senta@gmail.com>
2019-08-02 14:41:35 +0200

    algolia(setup): add setup in polyfills

--- apps/delivery/delivery/src/polyfills.ts
+++ apps/delivery/delivery/src/polyfills.ts
@@ -60,3 +60,11 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+/***************************************************************************************************
+ * ALGOLIA CONFIG
+ */
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
