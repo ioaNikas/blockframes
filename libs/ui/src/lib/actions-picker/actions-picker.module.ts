ae4a246b240948e5d737fb83022b88f022d82045
Laurent Senta <Laurent.Senta@gmail.com>
2019-08-07 16:07:48 +0200

    review(qa): fx layout in html

--- libs/ui/src/lib/actions-picker/actions-picker.module.ts
+++ libs/ui/src/lib/actions-picker/actions-picker.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ActionPickerItem, ActionsPickerComponent } from './actions-picker.component';
 import { MatRadioModule } from '@angular/material';
+import { FlexModule } from '@angular/flex-layout';
 
 // Share the type of items with our users.
 export { ActionPickerItem };
@@ -15,7 +16,7 @@ export { ActionPickerItem };
  */
 @NgModule({
   declarations: [ActionsPickerComponent],
-  imports: [MatListModule, MatIconModule, RouterModule, CommonModule, MatRadioModule],
+  imports: [MatListModule, MatIconModule, RouterModule, CommonModule, MatRadioModule, FlexModule],
   exports: [ActionsPickerComponent]
 })
 export class ActionsPickerModule {}
