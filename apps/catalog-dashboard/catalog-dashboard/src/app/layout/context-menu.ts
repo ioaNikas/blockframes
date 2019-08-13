a246b42f9d03f95bcc982c5022dad9a1568203ba
bruce <delorme.bruce.michel@gmail.com>
2019-08-13 15:41:36 +0200

    updated contextual menu

--- apps/catalog-dashboard/catalog-dashboard/src/app/layout/context-menu.ts
+++ apps/catalog-dashboard/catalog-dashboard/src/app/layout/context-menu.ts
@@ -3,14 +3,14 @@ export const CONTEXT_MENU = [
     route: 'default',
     items: [
       { name: 'home', path: '/layout/o/catalog/home' },
-      { name: 'import', path: '/layout/o/home/import' }, // temp until good place for this route is found
+      { name: 'Upload avails & films doc', path: '/layout/o/home/import' }, // temp until good place for this route is found
     ]
   },
   {
     route: '/layout/o/catalog/home',
     items: [
       { name: 'home', path: '/layout/o/catalog/home' },
-      { name: 'import', path: '/layout/o/home/import' }, // temp until good place for this route is found
+      { name: 'Upload avails & films doc', path: '/layout/o/home/import' }, // temp until good place for this route is found
     ]
   }
 ];
