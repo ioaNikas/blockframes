2bde4c1237a6278dc1ec33bdd79a732353dee2b9
Laurent Senta <Laurent.Senta@gmail.com>
2019-08-14 16:02:21 +0200

    backend(utils): fix imports

--- apps/backend-functions/src/utils.ts
+++ apps/backend-functions/src/utils.ts
@@ -24,10 +24,12 @@ export function onDocumentUpdate(docPath: string, fn: Function) {
   return functions.firestore.document(docPath).onUpdate(skipWhenRestoring(logErrors(fn)));
 }
 
+/** Same as onDocumentUpdate but with the max timeout possible (blockchain txs take time). */
 export function onOrganizationDocumentUpdate(docPath: string, fn: Function) {
-  return functions.runWith({timeoutSeconds: 540}).firestore // same as above but with the max timout possible for blockchain txs
-  .document(docPath)
-  .onUpdate(backup.skipWhenRestoring(fn));
+  return functions
+    .runWith({ timeoutSeconds: 540 })
+    .firestore.document(docPath)
+    .onUpdate(skipWhenRestoring(logErrors(fn)));
 }
 
 export function onDocumentCreate(docPath: string, fn: Function) {
@@ -42,6 +44,10 @@ export function onDocumentCreate(docPath: string, fn: Function) {
  * Checks properties of two material to tell if they are the same or not.
  */
 export function isTheSame(matA: Material, matB: Material): boolean {
-  const getProperties = ({value, description, category}: Material) => ({ value, description, category });
+  const getProperties = ({ value, description, category }: Material) => ({
+    value,
+    description,
+    category
+  });
   return JSON.stringify(getProperties(matA)) === JSON.stringify(getProperties(matB));
 }
