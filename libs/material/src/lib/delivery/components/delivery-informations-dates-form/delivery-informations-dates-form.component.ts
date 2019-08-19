0b66795bf11551b3422d031759ece8bab0a4603c
hugo-hontans <hugohontans@gmail.com>
2019-08-19 16:17:24 +0200

    can update delivery dates informations

--- libs/material/src/lib/delivery/components/delivery-informations-dates-form/delivery-informations-dates-form.component.ts
+++ libs/material/src/lib/delivery/components/delivery-informations-dates-form/delivery-informations-dates-form.component.ts
@@ -2,12 +2,18 @@ import {
   Component,
   ChangeDetectionStrategy,
 } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
 
 @Component({
-  selector: 'delivery-informations-dates-form',
+  selector: '[formGroup] delivery-informations-dates-form',
   templateUrl: './delivery-informations-dates-form.component.html',
   styleUrls: ['./delivery-informations-dates-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeliveryInformationsDatesFormComponent {
+  constructor(public controlContainer: ControlContainer) {}
+
+  public get control() {
+    return this.controlContainer.control;
+  }
 }
