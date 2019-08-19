8f83462d55962c07614cfb44cb88032148c20f39
hugo-hontans <hugohontans@gmail.com>
2019-08-19 12:24:28 +0200

    create delivery-informations-editable page

--- libs/material/src/lib/delivery/pages/delivery-informations-editable/delivery-informations-editable.component.ts
+++ libs/material/src/lib/delivery/pages/delivery-informations-editable/delivery-informations-editable.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Delivery, DeliveryQuery, DeliveryService } from '../../+state';
-import { tap, switchMap, startWith } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material';
-import { createInformationsFormGroup } from '../../forms/informations.form';
 
 @Component({
   selector: 'delivery-informations-editable',
@@ -12,43 +7,10 @@ import { createInformationsFormGroup } from '../../forms/informations.form';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DeliveryInformationsEditableComponent implements OnInit {
-  public opened = false;
-  public editContent = 'dates';
-
-  public informationsFormGroup = createInformationsFormGroup();
-
-  public delivery$: Observable<Delivery>;
 
   constructor(
-    private query: DeliveryQuery,
-    private service: DeliveryService,
-    private snackBar: MatSnackBar
-  ) {}
+    ) {}
 
   ngOnInit() {
-    this.delivery$ = this.query.selectActive().pipe(
-      tap(delivery => {
-        this.informationsFormGroup.patchValue(delivery);
-      }),
-      switchMap(delivery => this.informationsFormGroup.valueChanges.pipe(startWith(delivery)))
-    );
-  }
-
-  public openSidenav(name: string) {
-    this.editContent = name;
-    this.opened = true;
-  }
-
-  public async updateInformations() {
-    // TODO: update guaranteed minimum payment deadline informations: issue#764
-    try {
-      if (this.informationsFormGroup.invalid) {
-        throw new Error('Delivery informations are not valid');
-      }
-      await this.service.updateInformations(this.informationsFormGroup.value);
-      this.snackBar.open('Informations updated', 'close', { duration: 2000 });
-    } catch (error) {
-      this.snackBar.open(error.message, 'close', { duration: 2000 });
-    }
   }
 }
