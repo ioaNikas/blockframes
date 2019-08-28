907d3d93b8019b2755c44d49c8b4c5930079f452
bruce <delorme.bruce.michel@gmail.com>
2019-08-28 16:10:28 +0200

    added left sidenav

--- libs/ui/src/lib/toolbar/left-menu/left-menu.component.ts
+++ libs/ui/src/lib/toolbar/left-menu/left-menu.component.ts
@@ -2,14 +2,15 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 
 @Component({
-  selector: 'toolbar-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: 'toolbar-left-menu',
+  templateUrl: './left-menu.component.html',
+  styleUrls: ['./left-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
+export class LeftMenuComponent {
 
   @Input() sidenav: MatSidenav
 
   constructor() {}
+
 }
