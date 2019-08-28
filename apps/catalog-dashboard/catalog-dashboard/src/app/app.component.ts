f7104c445b46da28493c283fe47681c07556d405
bruce <delorme.bruce.michel@gmail.com>
2019-08-28 16:06:11 +0200

    added comment when using IconComponent

--- apps/catalog-dashboard/catalog-dashboard/src/app/app.component.ts
+++ apps/catalog-dashboard/catalog-dashboard/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 import { AuthQuery } from '@blockframes/auth';
+import { IconComponent } from '@blockframes/ui';
 
 @Component({
   selector: 'catalog-dashboard-root',
@@ -7,7 +8,10 @@ import { AuthQuery } from '@blockframes/auth';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  constructor(private query: AuthQuery) { }
+  constructor(
+    private query: AuthQuery,
+    private icons: IconComponent, // even if not used in component, keep this to load icons
+  ) { }
 
   @HostListener('window:beforeunload', ['$event'])
   closeProtection($event: any) {
