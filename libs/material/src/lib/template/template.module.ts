f3321fee0f25778e9f8c528c106ff367e429e751
Mussuranae <mandonnetdev@outlook.com>
2019-08-30 17:43:50 +0200

    add table instaed of card for template list

--- libs/material/src/lib/template/template.module.ts
+++ libs/material/src/lib/template/template.module.ts
@@ -30,7 +30,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { TemplateListGuard } from './guards/template-list.guard';
-import { MatTableModule, MatSelectModule, MatSortModule } from '@angular/material';
+import { MatSelectModule, MatSortModule } from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table'
 
 const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: 'full' },
@@ -83,6 +85,7 @@ const routes: Routes = [
     MatTableModule,
     MatSelectModule,
     MatSortModule,
+    MatCheckboxModule,
     RouterModule.forChild(routes)
   ],
   entryComponents: [TemplateAddComponent]
