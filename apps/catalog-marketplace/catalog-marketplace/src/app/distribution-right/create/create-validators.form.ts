c16b7b85f22b2419c3c7a7898531efa41ff26836
Maxstgt <max.j.f@gmx.de>
2019-08-30 12:07:55 +0200

    working on review

--- apps/catalog-marketplace/catalog-marketplace/src/app/distribution-right/create/create-validators.form.ts
+++ apps/catalog-marketplace/catalog-marketplace/src/app/distribution-right/create/create-validators.form.ts
@@ -1,9 +1,7 @@
 import { AbstractControl } from '@angular/forms';
 import { staticModels } from '@blockframes/movie';
 
-export function createLanguageValidator(
-  control: AbstractControl
-): { [key: string]: boolean } | null {
+export function createLanguageValidator(control: AbstractControl): { [key: string]: boolean } | null {
   const movieLanguageToLowerCase = staticModels['LANGUAGES']
     .map(key => key.label)
     .map(language => language.toLowerCase());
