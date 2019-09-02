77daa3c5d1842817fd04acf823ed317014978a76
Laurent Senta <Laurent.Senta@gmail.com>
2019-09-02 17:04:37 +0200

    migrations: prep toronto

--- apps/backend-ops/src/main.ts
+++ apps/backend-ops/src/main.ts
@@ -1,7 +1,8 @@
-import { prepareFirebase } from './firebaseSetup';
+import { createAllUsers, prepareFirebase } from './firebaseSetup';
 import { MIGRATIONS } from './firestoreMigrations';
 import { updateDBVersion } from './migrations';
 import { loadAdminServices } from './admin';
+import { USERS } from './users.toronto.fixture';
 
 async function prepareForTesting() {
   console.info('Preparing firebase...');
@@ -16,6 +17,15 @@ async function migrateToV1() {
   const { db } = loadAdminServices();
   await MIGRATIONS['1'].upgrade(db);
   await updateDBVersion(db, 1);
+  process.exit(0);
+}
+
+async function prepareToronto() {
+  const { auth } = loadAdminServices();
+  console.info('create all users...');
+  await createAllUsers(USERS, auth);
+  console.info('done.');
+  process.exit(0);
 }
 
 const args = process.argv.slice(2);
@@ -25,4 +35,6 @@ if (cmd === 'prepareForTesting') {
   prepareForTesting();
 } else if (cmd === 'migrateToV1') {
   migrateToV1();
+} else if (cmd === 'prepareToronto') {
+  prepareToronto();
 }
