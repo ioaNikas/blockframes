246d79713b439692f333ae12c1aa86450b517742
hugo-hontans <hugohontans@gmail.com>
2019-09-18 15:22:03 +0200

    add methods form FormEntity to FormParty

--- libs/utils/src/lib/form/forms/batch.form.ts
+++ libs/utils/src/lib/form/forms/batch.form.ts
@@ -2,17 +2,37 @@ import { FormGroup, AbstractControl } from '@angular/forms';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { map, filter, startWith } from 'rxjs/operators';
 
-type BatchControl<E = any> = {
+type EntityControl<E = any> = {
   [key in keyof Partial<E>]: AbstractControl;
 }
 
-export class FormParty<C extends BatchControl> extends FormGroup {
+export class FormParty<C extends EntityControl> extends FormGroup {
   get<K extends keyof C>(path: Extract<K, string>): C[K] {
     return super.get(path) as C[K];
   }
+
+  addControl<K extends keyof C>(name: Extract<K, string>, control: C[K]) {
+    super.addControl(name, control);
+  }
+
+  removeControl(name: Extract<keyof C, string>) {
+    super.removeControl(name);
+  }
+
+  setControl<K extends keyof C>(name: Extract<K, string>, control: C[K]) {
+    super.setControl(name, control);
+  }
+
+  setValue(value: Partial<C>, options?: { onlySelf?: boolean; emitEvent?: boolean }) {
+    super.setValue(value, options);
+  }
+
+  patchValue(entity: Partial<C>, options?: { onlySelf?: boolean; emitEvent?: boolean }) {
+    super.patchValue(entity, options);
+  }
 }
 
-export abstract class FormBatch<E, C extends BatchControl<E>> extends FormGroup {
+export abstract class FormBatch<E, C extends EntityControl<E>> extends FormGroup {
   protected idKey = 'id';
   private active = new BehaviorSubject<string>(null);
   public active$ = this.active.asObservable();
